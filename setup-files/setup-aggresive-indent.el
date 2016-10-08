c6e966434c79c85368ba8575d71a08d5935981e6
C S Raghunandan <rnraghunandan@gmail.com>
2016-10-08 08:29:26 +0530

    fix annoying line jumping when `;` not been entered - aggressive-indent-mode

--- setup-files/setup-aggresive-indent.el
+++ setup-files/setup-aggresive-indent.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-10-06 22:28:45 csraghunandan>
+;; Time-stamp: <2016-10-08 08:29:17 csraghunandan>
 
 ;; aggressive-indent
 ;; https://github.com/Malabarba/aggressive-indent-mode
@@ -9,6 +9,16 @@
 (use-package aggressive-indent
   :config
   (add-to-list 'aggressive-indent-excluded-modes 'web-mode)
+
+  ;; The variable aggressive-indent-dont-indent-if lets you customize when you don't want indentation to
+  ;; happen. For instance, if you think it's annoying that lines jump around in c++-mode
+  ;; because you haven't typed the `;' do this :-
+  (add-to-list
+   'aggressive-indent-dont-indent-if
+   '(and (derived-mode-p 'css-mode 'js2-mode 'rust-mode)
+         (null (string-match "\\([;{}]\\|\\b\\(if\\|for\\|while\\)\\b\\)"
+                             (thing-at-point 'line)))))
+
   (global-aggressive-indent-mode))
 
 (provide 'setup-aggresive-indent)
