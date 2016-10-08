68f23246e8faa4d69aeeea211950c6448fee9eda
C S Raghunandan <rnraghunandan@gmail.com>
2016-10-08 08:40:23 +0530

    Add page-break-lines-mode hook to prog-mode

--- setup-files/setup-page-break-lines.el
+++ setup-files/setup-page-break-lines.el
@@ -1,10 +1,12 @@
-;; Time-stamp: <2016-10-08 08:24:04 csraghunandan>
+;; Time-stamp: <2016-10-08 08:40:12 csraghunandan>
 
 ;; Show actual lines instead of the page break char ^L
 ;; https://github.com/purcell/page-break-lines
 
 (use-package page-break-lines :defer 2
-  :diminish page-break-lines-mode)
+  :diminish page-break-lines-mode
+  :config
+  (add-hook 'prog-mode-hook (lambda() (turn-on-page-break-lines-mode))))
 
 (provide 'setup-page-break-lines)
 
