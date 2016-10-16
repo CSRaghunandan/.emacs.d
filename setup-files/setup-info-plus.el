ad7ab29e572ddb83aa16a9b3acfd6d8b624c24ea
C S Raghunandan <rnraghunandan@gmail.com>
2016-10-16 16:06:23 +0530

    Added setup info+

---
+++ setup-files/setup-info-plus.el
@@ -0,0 +1,9 @@
+;; Time-stamp: <2016-10-16 15:28:33 csraghunandan>
+
+;; extend the standard `info.el' emacs library
+;; https://www.emacswiki.org/emacs/InfoPlus
+
+(use-package info+
+  :config (require 'info+))
+
+(provide 'setup-info-plus)
