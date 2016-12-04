0f1dc4ba149d56d1a8fe431548e0b2b9fadf25d5
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-04 12:51:38 +0530

    Add hydra for macro. Add to init.el after learing to use macros

---
+++ setup-files/setup-macro.el
@@ -0,0 +1,38 @@
+(defhydra hydra-macro (:hint nil :color pink :pre
+                             (when defining-kbd-macro
+                                 (kmacro-end-macro 1)))
+  "
+  ^Create-Cycle^   ^Basic^           ^Insert^        ^Save^         ^Edit^
+╭─────────────────────────────────────────────────────────────────────────╯
+     ^_p_^           [_e_] execute    [_i_] insert    [_b_] name      [_'_] previous
+     ^^↑^^           [_d_] delete     [_t_] set       [_K_] key       [_,_] last
+ _b_ ←   → _f_     [_o_] edit       [_a_] add       [_x_] register
+     ^^↓^^           [_r_] region     [_F_] format    [_B_] defun
+     ^_n_^           [_m_] step
+    ^^   ^^          [_s_] swap
+"
+  ("b" kmacro-start-macro :color blue)
+  ("f" kmacro-end-or-call-macro-repeat)
+  ("p" kmacro-cycle-ring-previous)
+  ("n" kmacro-cycle-ring-next)
+  ("r" apply-macro-to-region-lines)
+  ("d" kmacro-delete-ring-head)
+  ("e" kmacro-end-or-call-macro-repeat)
+  ("o" kmacro-edit-macro-repeat)
+  ("m" kmacro-step-edit-macro)
+  ("s" kmacro-swap-ring)
+  ("i" kmacro-insert-counter)
+  ("t" kmacro-set-counter)
+  ("a" kmacro-add-counter)
+  ("F" kmacro-set-format)
+  ("b" kmacro-name-last-macro)
+  ("K" kmacro-bind-to-key)
+  ("B" insert-kbd-macro)
+  ("x" kmacro-to-register)
+  ("'" kmacro-edit-macro)
+  ("," edit-kbd-macro)
+  ("q" nil :color blue))
+
+(bind-key "C-c h M" 'hydra-macro/body)
+
+(provide 'setup-macro)
