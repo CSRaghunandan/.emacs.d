4c573316b42882aa7edd882d3b6054a4bb261c0e
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-04 12:48:27 +0530

    Add structured-haskell-mode

--- setup-files/setup-haskell.el
+++ setup-files/setup-haskell.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-12-03 12:12:54 csraghunandan>
+;; Time-stamp: <2016-12-04 12:44:01 csraghunandan>
 
 ;; haskell-mode configuration
 ;; https://github.com/haskell/haskell-mode
@@ -29,7 +29,14 @@
       :config
       (add-hook 'haskell-mode-hook #'hindent-mode)
       ;; reformat the buffer using hindent on save
-      (setq hindent-reformat-buffer-on-save t))))
+      (setq hindent-reformat-buffer-on-save t)))
+
+  (add-to-list 'load-path "/Users/csraghunandan/structured-haskell-mode/elisp")
+  (require 'shm)
+  (add-hook 'haskell-mode-hook (lambda ()
+                                 (haskell-indentation-mode -1)
+                                 (smartparens-mode -1)))
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode))
 
 (provide 'setup-haskell)
 
