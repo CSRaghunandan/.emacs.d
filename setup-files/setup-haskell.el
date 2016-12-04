3d7044d834b0f4bee09d5cfbd1b20b24798549d4
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-04 20:56:55 +0530

    Added use-package macro for shm. Add more documentation

--- setup-files/setup-haskell.el
+++ setup-files/setup-haskell.el
@@ -1,12 +1,8 @@
-;; Time-stamp: <2016-12-04 12:44:01 csraghunandan>
+;; Time-stamp: <2016-12-04 20:56:39 csraghunandan>
 
 ;; haskell-mode configuration
 ;; https://github.com/haskell/haskell-mode
 (use-package haskell-mode
-  ;; haskell-mode swaps `C-m' and `C-j' behavior. Revert it back
-  :bind (:map haskell-mode-map
-              ("C-m" . newline)
-              ("C-j" . electric-newline-and-maybe-indent))
   :config
   (defun my-haskell-mode-hook ()
     "Hook for `haskell-mode'."
@@ -31,12 +27,20 @@
       ;; reformat the buffer using hindent on save
       (setq hindent-reformat-buffer-on-save t)))
 
-  (add-to-list 'load-path "/Users/csraghunandan/structured-haskell-mode/elisp")
-  (require 'shm)
-  (add-hook 'haskell-mode-hook (lambda ()
-                                 (haskell-indentation-mode -1)
-                                 (smartparens-mode -1)))
-  (add-hook 'haskell-mode-hook 'structured-haskell-mode))
+  ;; structured-haskell-mode - paredit like features for haskell code
+  ;; https://github.com/chrisdone/structured-haskell-mode
+  (use-package shm
+    :config
+    (add-hook 'haskell-mode-hook (lambda ()
+                                   ;; shm has it's own indention rules. No need
+                                   ;; of `haskell-indentation-mode'
+                                   (haskell-indentation-mode -1)
+                                   ;; shm conflicts with smartparens. Hence, disable.
+                                   (smartparens-mode -1)))
+    (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+    ;; add case splits for simple sum types
+    (require 'shm-case-split)
+    (bind-key "C-c |" 'shm/case-split haskell-mode-map)))
 
 (provide 'setup-haskell)
 
@@ -48,3 +52,16 @@
 ;; `M-,' jump back from definition
 ;; `M-q' to format the expression at point using hindent
 ;; `C-M-\' to format the selected region using hindent
+
+;; structured-haskell-mode
+;; `C-j' will automatically add a comma when inside a ilst
+;; `C-+' will will look at the current node and add another operand in the direction the cursor is leaning towards
+;; automatically reindents code as we type(similar to aggressive indent)
+;; multi line strings: press `C-j' while inside a string to split it to next line
+;; `C-c |' to add case splits for simple sum types
+;; `C-k' will act like `sp-kill-hybrid-sexp'
+;; `)' will move cursor to the end of  the parent
+;; Skeletons: Typing prefixes of common syntax will auto-fill in the structure
+;;            with "slots" that you can hit TAB to jump to, which auto-disappear
+;;            when you type in them
+;;
