db294cae0293d3994d9c6ebde2c777d7b1867735
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-09 20:39:34 +0530

    Make hydras more readable on darker zenburn background

--- setup-files/setup-hydra.el
+++ setup-files/setup-hydra.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-12-09 12:42:52 csraghunandan>
+;; Time-stamp: <2016-12-09 18:20:33 csraghunandan>
 
 ;; hydra :- This is a package for GNU Emacs that can be used to tie related
 ;; commands into a family of short bindings with a common prefix - a Hydra.
@@ -10,10 +10,10 @@
 (use-package hydra
   :config
   (set-face-attribute 'hydra-face-red      nil :foreground "Red"        :bold t)
-  (set-face-attribute 'hydra-face-blue     nil :foreground "cadetblue1" :bold t)
+  (set-face-attribute 'hydra-face-blue     nil :foreground "Cyan" :bold t)
   (set-face-attribute 'hydra-face-amaranth nil :foreground "#e52b50"    :bold t)
   (set-face-attribute 'hydra-face-pink     nil :foreground "HotPink1"   :bold t)
-  (set-face-attribute 'hydra-face-teal     nil :foreground "#367588"    :bold t)
+  (set-face-attribute 'hydra-face-teal     nil :foreground "SkyBlue1"    :bold t)
   (hydra-add-font-lock))
 
 (provide 'setup-hydra)
