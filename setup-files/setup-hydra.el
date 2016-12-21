f29cfa8038f5ac554d3c02a9452c67ee3fd2b7e6
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-21 15:46:48 +0530

    clean up setup-hydra.el. Make hydra-face-red more visible on zenburn theme

--- setup-files/setup-hydra.el
+++ setup-files/setup-hydra.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-12-09 18:20:33 csraghunandan>
+;; Time-stamp: <2016-12-21 15:35:12 csraghunandan>
 
 ;; hydra :- This is a package for GNU Emacs that can be used to tie related
 ;; commands into a family of short bindings with a common prefix - a Hydra.
@@ -6,14 +6,15 @@
 
 ;; hydras: flycheck, ibuffer, fold, this, move, yasnippet, info, macros
 ;;         dired sort, bm, projectile, toggle case, langtools, kruecolor
+;;         smartparens, multi-term
 
 (use-package hydra
   :config
-  (set-face-attribute 'hydra-face-red      nil :foreground "Red"        :bold t)
-  (set-face-attribute 'hydra-face-blue     nil :foreground "Cyan" :bold t)
-  (set-face-attribute 'hydra-face-amaranth nil :foreground "#e52b50"    :bold t)
-  (set-face-attribute 'hydra-face-pink     nil :foreground "HotPink1"   :bold t)
-  (set-face-attribute 'hydra-face-teal     nil :foreground "SkyBlue1"    :bold t)
+  (set-face-attribute 'hydra-face-red nil :foreground "#FF6956" :bold t)
+  (set-face-attribute 'hydra-face-blue nil :foreground "Cyan" :bold t)
+  (set-face-attribute 'hydra-face-amaranth nil :foreground "#e52b50" :bold t)
+  (set-face-attribute 'hydra-face-pink nil :foreground "HotPink1" :bold t)
+  (set-face-attribute 'hydra-face-teal nil :foreground "SkyBlue1" :bold t)
   (hydra-add-font-lock))
 
 (provide 'setup-hydra)
