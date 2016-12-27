63f801f362695baaec3ed115701cf61715b6b9d0
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-27 16:27:16 +0530

    Add realgud package

---
+++ setup-files/setup-realgud.el
@@ -0,0 +1,8 @@
+;; Time-stamp: <2016-12-27 16:26:54 csraghunandan>
+
+;; realgud: Debugger interface for emacs
+;; https://github.com/realgud/realgud/
+(use-package realgud)
+
+;; to load realgud, do `load-library' RET `realgud'
+;; start the debugger you want with `realgud:debugger'
