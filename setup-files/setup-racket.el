6686d39c137a6a6da0dcfcb3ef9967808961d3e7
CSRaghunandan <rnraghunandan@gmail.com>
2016-12-30 18:31:31 +0530

    remove company-quickhelp in setup-racket

--- setup-files/setup-racket.el
+++ setup-files/setup-racket.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2016-12-30 18:04:54 csraghunandan>
+;; Time-stamp: <2016-12-30 18:30:50 csraghunandan>
 
 ;; racket-mode
 ;; https://github.com/greghendershott/racket-mode
@@ -6,12 +6,10 @@
   :config
   (defun my-racket-mode-hook ()
     (set (make-local-variable 'company-backends)
-         '((company-capf company-dabbrev-code company-files company-yasnippet)))
-    (company-quickhelp-mode 0))
+         '((company-capf company-dabbrev-code company-files company-yasnippet))))
   (defun my-racket-mode-hook2 ()
     (set (make-local-variable 'company-backends)
-         '((company-capf company-dabbrev-code company-yasnippet)))
-    (company-quickhelp-mode 0))
+         '((company-capf company-dabbrev-code company-yasnippet))))
   (add-hook 'racket-mode-hook 'my-racket-mode-hook)
   (add-hook 'racket-mode-hook 'company-mode)
   (add-hook 'racket-repl-mode-hook 'my-racket-mode-hook2)
