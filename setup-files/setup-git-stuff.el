3fb42cda91e02b94fb22c055a43ba251c22b3660
CSRaghunandan <rnraghunandan@gmail.com>
2017-01-03 15:56:07 +0530

    Enable diff-hl-mode for prog-modes

--- setup-files/setup-git-stuff.el
+++ setup-files/setup-git-stuff.el
@@ -1,12 +1,12 @@
-;; Time-stamp: <2017-01-02 20:37:55 csraghunandan>
+;; Time-stamp: <2017-01-03 15:53:53 csraghunandan>
 
 ;; magit, git-timemachine, diff-hl
 
 ;; https://magit.vc , https://github.com/magit/magit
 ;; magit - the git porcelain to manage git
-(use-package magit :defer t
+(use-package magit :defer 1
   :bind (("C-c m s" . magit-status)
-          ("C-c m b"   . magit-blame))
+          ("C-c m b" . magit-blame))
   :config (setq magit-completing-read-function 'ivy-completing-read))
 
 ;; git-timemachine - to rollback to different commits of files
@@ -18,7 +18,11 @@
 ;; diff-hl - highlight diffs in the fringe
 ;; https://github.com/dgutov/diff-hl
 (use-package diff-hl :defer 1
-  :config (add-hook 'dired-mode-hook #'diff-hl-dired-mode))
+  :config
+  (add-hook 'dired-mode-hook #'diff-hl-dired-mode)
+  (add-hook 'prog-mode-hook #'diff-hl-mode)
+  ;; integate diff-hl with magit
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 ;; git-messenger: popup commit message at current line
 ;; https://github.com/syohex/emacs-git-messenger
@@ -31,3 +35,9 @@
   (bind-key "m" 'git-messenger:copy-message git-messenger-map))
 
 (provide 'setup-git-stuff)
+
+;; diff-hl
+;; C-x v [ -> diff-hl-previous-hunk
+;; C-x v ] -> diff-hl-next-hunk
+;; C-x v = -> diff-hl-goto-hunk
+;; C-x v n -> diff-hl-revert-hunk
