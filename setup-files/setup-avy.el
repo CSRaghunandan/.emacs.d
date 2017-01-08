99df45089814b9329f3ad0c241ff31c1cce03879
CSRaghunandan <rnraghunandan@gmail.com>
2017-01-08 12:37:08 +0530

    Simplify and make mode-line more consistent and easy on the eyes

--- setup-files/setup-avy.el
+++ setup-files/setup-avy.el
@@ -1,28 +1,25 @@
-;; Time-stamp: <2017-01-05 14:51:28 csraghunandan>
+;; Time-stamp: <2017-01-08 11:55:35 csraghunandan>
 
 ;; avy, avy-zap
 
-;; avy
+;; avy - Emacs package for jumping to visible text using character based decision tree
 ;; https://github.com/abo-abo/avy
-;; Emacs package for jumping to visible text using character based decision tree
 (use-package avy
-  :init
+  :bind ("C-`" . avy-goto-word-1)
+  :bind* ("C-'" . avy-goto-char-timer)
+  :config
   (setq avy-keys-alist
         `((avy-goto-char-timer . (?a ?s ?d ?f ?g ?h ?j ?k ?l))))
-  (setq avy-style 'pre)
-  :bind (("C-'" . avy-goto-char-timer)
-          ("C-`" . avy-goto-word-1)))
+  (setq avy-style 'pre))
 
-;; avy-zap
+;; avy-zap - Use avy frontend to yank till the match entered by user
 ;; https://github.com/cute-jumper/avy-zap
-;; Use avy frontend to yank till the match entered by user
 (use-package avy-zap
   :bind (("M-z" . avy-zap-to-char-dwim))
   :config (advice-add 'swiper :before 'avy-push-mark))
 
-;; ace-link
+;; ace-link - quickly traverse through links in info
 ;; https://github.com/abo-abo/ace-link
-;; quickly traverse through links in buffers
 (use-package ace-link
   :config (ace-link-setup-default))
 
--- setup-files/setup-zenburn.el
+++ setup-files/setup-zenburn.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-01-07 22:44:50 csraghunandan>
+;; Time-stamp: <2017-01-08 12:36:57 csraghunandan>
 
 ;; Theme configuration for emacs
 ;; https://github.com/bbatsov/zenburn-emacs
@@ -13,6 +13,7 @@
   ;; make function face brighter so it's easily distinguishable
   (set-face-attribute 'font-lock-function-name-face nil
                       :foreground "CadetBlue1")
+
   ;; fontify links to make them standout
   (set-face-attribute 'link nil
                       :foreground "#C9B8A2"
@@ -20,6 +21,7 @@
   (set-face-attribute 'link-visited nil
                       :foreground "#C9AE8C"
                       :underline nil :weight 'normal)
+
   ;; make everything look gray :)
   (set-face-attribute 'font-lock-comment-delimiter-face nil
                       :foreground "gray55")
@@ -27,33 +29,33 @@
                       :foreground "gray55" :slant 'italic)
   (set-face-attribute 'font-lock-doc-face nil
                       :foreground "gray70" :slant 'italic)
-  (set-face-background 'shm-current-face "#444444")
+  (set-face-background 'shm-current-face "#404040")
   (set-face-attribute 'hl-line nil
-                      :background "#444444")
+                      :background "#404040")
   (set-face-attribute 'fringe nil
-                      :background "#444444")
+                      :background "#404040")
   (set-face-attribute 'vhl/default-face nil
-                      :background "#444444")
+                      :background "#404040")
 
   ;; make mode line look pretty :)
   (set-face-attribute 'mode-line nil
-                      :box '(:line-width 2 :color "gray30")
-                      :weight 'normal :foreground "#C8F7C8"
-                      :background "gray20")
+                      :box '(:line-width 2 :color "gray26")
+                      :weight 'bold :foreground "#BFEBBF"
+                      :background "gray26")
   (set-face-attribute 'mode-line-inactive nil
-                      :box '(:line-width 2 :color "gray30")
+                      :box '(:line-width 2 :color "gray20")
                       :weight 'normal :foreground "gray70")
   (set-face-attribute 'powerline-active2 nil
-                      :background "gray32")
+                      :background "gray26")
   (set-face-attribute 'powerline-active1 nil
-                      :background "gray32" :weight 'normal)
+                      :background "gray26" :weight 'bold)
   (set-face-attribute 'mode-line-buffer-id nil
                       :foreground "#FFECBA" :weight 'bold)
   ;; dim inactive modeline
   (set-face-attribute 'powerline-inactive2 nil
                       :background "gray20")
   (set-face-attribute 'powerline-inactive1 nil
-                      :background "gray32")
+                      :background "gray20")
 
   ;; contrasting colors for ivy minibuffer match results
   (set-face-attribute 'ivy-minibuffer-match-face-4 nil
