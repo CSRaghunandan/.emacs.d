19827384954547dfb1aee017f0761504a761e51c
CSRaghunandan <rnraghunandan@gmail.com>
2017-02-07 11:19:35 +0530

    fix binding to wrong undefined function

--- setup-files/setup-region-bindings-mode.el
+++ setup-files/setup-region-bindings-mode.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-02-07 10:58:56 csraghunandan>
+;; Time-stamp: <2017-02-07 11:19:23 csraghunandan>
 
 ;; Region Bindings Mode
 ;; https://github.com/fgallina/region-bindings-mode
@@ -16,7 +16,7 @@
    ("w" . kill-region)
    ("c" . copy-region-as-kill)
    ("d" . duplicate-current-line-or-region)
-   ("W" . rag/narrow-or-widen-dwim)
+   ("W" . recursive-narrow-or-widen-dwim)
    ("E" . eval-region)
    ("e" . er/expand-region)
    ("f" . fill-region)
