df77e32dcd2097f0922d6f2296a88075d5996122
CSRaghunandan <rnraghunandan@gmail.com>
2017-02-07 11:24:35 +0530

    remove unnecessary code for multiple-cursors which was causing a visual bug when narrowed buffers

--- setup-files/setup-mc.el
+++ setup-files/setup-mc.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-01-08 15:05:37 csraghunandan>
+;; Time-stamp: <2017-02-07 11:24:27 csraghunandan>
 
 ;; Multiple Cursors
 ;; https://github.com/magnars/multiple-cursors.el
@@ -7,6 +7,7 @@
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
          ("C-x C-m" . mc/mark-all-dwim))
+
   :bind (:map region-bindings-mode-map
               ("a" . mc/mark-all-like-this)
               ("p" . mc/mark-previous-like-this)
@@ -21,37 +22,9 @@
               ("#" . mc/insert-numbers) ; use num prefix to set the starting number
               ("^" . mc/edit-beginnings-of-lines)
               ("$" . mc/edit-ends-of-lines))
-  :init
-  (setq mc/list-file (locate-user-emacs-file "mc-lists"))
+  :init (setq mc/list-file (locate-user-emacs-file "mc-lists"))
 
-  ;; Disable the annoying sluggish matching paren blinks for all cursors
-  ;; when you happen to type a ")" or "}" at all cursor locations.
-  (defvar modi/mc-blink-matching-paren--store nil
-    "Internal variable used to restore the value of `blink-matching-paren'
-after `multiple-cursors-mode' is quit.")
-
-  ;; The `multiple-cursors-mode-enabled-hook' and
-  ;; `multiple-cursors-mode-disabled-hook' are run in the
-  ;; `multiple-cursors-mode' minor mode definition, but they are not declared
-  ;; (not `defvar'd). So do that first before using `add-hook'.
-  (defvar multiple-cursors-mode-enabled-hook nil
-    "Hook that is run after `multiple-cursors-mode' is enabled.")
-  (defvar multiple-cursors-mode-disabled-hook nil
-    "Hook that is run after `multiple-cursors-mode' is disabled.")
-
-  (defun modi/mc-when-enabled ()
-    "Function to be added to `multiple-cursors-mode-enabled-hook'."
-    (setq modi/mc-blink-matching-paren--store blink-matching-paren)
-    (setq blink-matching-paren nil))
-
-  (defun modi/mc-when-disabled ()
-    "Function to be added to `multiple-cursors-mode-disabled-hook'."
-    (setq blink-matching-paren modi/mc-blink-matching-paren--store))
-
-  (add-hook 'multiple-cursors-mode-enabled-hook #'modi/mc-when-enabled)
-  (add-hook 'multiple-cursors-mode-disabled-hook #'modi/mc-when-disabled)
-
-  :config (add-hook 'prog-mode-hook 'multiple-cursors-mode))
+  :config (add-hook 'prog-mode-hook #'multiple-cursors-mode))
 
 (provide 'setup-mc)
 
