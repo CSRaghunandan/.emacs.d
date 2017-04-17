f7923bd7580da0361480b0b70267161bd9bb18ca
CSRaghunandan <rnraghunandan@gmail.com>
2017-04-18 00:01:13 +0530

    Add modi/messages-auto-tail to setup-buffers.el

--- setup-files/setup-buffers.el
+++ setup-files/setup-buffers.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-04-17 23:58:09 csraghunandan>
+;; Time-stamp: <2017-04-18 00:00:06 csraghunandan>
 
 ;; configuration for buffers
 
@@ -39,6 +39,29 @@ Example: M-644 M-x modi/set-file-permissions."
     (message "%s" cmd)
     (shell-command cmd "*Shell Temp*")
     (kill-buffer "*Shell Temp*")))
+
+;;; *Messages* Auto-tail
+;; Improved upon http://stackoverflow.com/a/4685005/1219634
+(defun modi/messages-auto-tail (&rest _)
+  "Make *Messages* buffer auto-scroll to the end after each message."
+  (let* ((buf-name "*Messages*")
+         ;; Create *Messages* buffer if it does not exist
+         (buf (get-buffer-create buf-name)))
+    ;; Activate this advice only if the point is _not_ in the *Messages* buffer
+    ;; to begin with. This condition is required; otherwise you will not be
+    ;; able to use `isearch' and other stuff within the *Messages* buffer as
+    ;; the point will keep moving to the end of buffer :P
+    (when (not (string= buf-name (buffer-name)))
+      ;; Go to the end of buffer in all *Messages* buffer windows that are
+      ;; *live* (`get-buffer-window-list' returns a list of only live windows).
+      (dolist (win (get-buffer-window-list buf-name nil :all-frames))
+        (with-selected-window win
+          (goto-char (point-max))))
+      ;; Go to the end of the *Messages* buffer even if it is not in one of
+      ;; the live windows.
+      (with-current-buffer buf
+        (goto-char (point-max))))))
+(advice-add 'message :after #'modi/messages-auto-tail)
 
 (defun rag/copy-buffer-file-name-as-kill (choice)
   "Copy the buffer-file-name to the kill-ring"
