6b9240b725eca8a79b9453c3571fa4f4b304065e
CSRaghunandan <rnraghunandan@gmail.com>
2017-05-08 11:35:13 +0530

    Archive subtrees under the same hierarchy as original in the archive files

--- setup-files/setup-org.el
+++ setup-files/setup-org.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-05-03 13:02:28 csraghunandan>
+;; Time-stamp: <2017-05-08 11:35:01 csraghunandan>
 
 ;; Org-mode configuration - Make sure you install the latest org-mode with `M-x' RET `org-plus-contrib'
 ;; http://orgmode.org/
@@ -387,6 +387,45 @@ text and copying to the killring."
   ("m" org-timer)
   ("t" org-timer-item)
   ("z" (org-info "Timers"))))
+
+(defadvice org-archive-subtree (around fix-hierarchy activate)
+  (let* ((fix-archive-p (and (not current-prefix-arg)
+                             (not (use-region-p))))
+         (afile (org-extract-archive-file (org-get-local-archive-location)))
+         (buffer (or (find-buffer-visiting afile) (find-file-noselect afile))))
+    ad-do-it
+    (when fix-archive-p
+      (with-current-buffer buffer
+        (goto-char (point-max))
+        (while (org-up-heading-safe))
+        (let* ((olpath (org-entry-get (point) "ARCHIVE_OLPATH"))
+               (path (and olpath (split-string olpath "/")))
+               (level 1)
+               tree-text)
+          (when olpath
+            (org-mark-subtree)
+            (setq tree-text (buffer-substring (region-beginning) (region-end)))
+            (let (this-command) (org-cut-subtree))
+            (goto-char (point-min))
+            (save-restriction
+              (widen)
+              (-each path
+                (lambda (heading)
+                  (if (re-search-forward
+                       (rx-to-string
+                        `(: bol (repeat ,level "*") (1+ " ") ,heading)) nil t)
+                      (org-narrow-to-subtree)
+                    (goto-char (point-max))
+                    (unless (looking-at "^")
+                      (insert "\n"))
+                    (insert (make-string level ?*)
+                            " "
+                            heading
+                            "\n"))
+                  (cl-incf level)))
+              (widen)
+              (org-end-of-subtree t t)
+              (org-paste-subtree level tree-text))))))))
 
 ;; A journaling tool with org-mode: `org-journal'
 ;; https://github.com/bastibe/org-journal
