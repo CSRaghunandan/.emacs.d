3d38fa1052971e113aa7d057c52747181cd61a73
CSRaghunandan <rnraghunandan@gmail.com>
2017-05-08 10:59:00 +0530

    Save write-protected files with sudo automatically

--- setup-files/setup-editing.el
+++ setup-files/setup-editing.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-03-09 22:09:10 csraghunandan>
+;; Time-stamp: <2017-05-08 10:58:49 csraghunandan>
 
 ;;; configuration for all the editing stuff in emacs
 ;; Kill ring
@@ -537,5 +537,34 @@ this command."
       (flush-lines "^\\s-*$" (region-beginning) (region-end)))
     do-not-run-orig-fn))
 (advice-add 'delete-blank-lines :before-until #'modi/delete-blank-lines-in-region)
+
+(defadvice basic-save-buffer-2 (around fix-unwritable-save-with-sudo activate)
+  "When we save a buffer which is write-protected, try to sudo-save it.
+
+When the buffer is write-protected it is usually opened in
+read-only mode.  Use \\[read-only-mode] to toggle
+`read-only-mode', make your changes and \\[save-buffer] to save.
+Emacs will warn you that the buffer is write-protected and asks
+you to confirm if you really want to save.  If you answer yes,
+Emacs will use sudo tramp method to save the file and then
+reverts it, making it read-only again.  The buffer stays
+associated with the original non-sudo filename."
+           (condition-case err
+               (progn
+                 ad-do-it)
+             (file-error
+              (when (string-prefix-p
+                     "Doing chmod: operation not permitted"
+                     (error-message-string err))
+                (let ((old-buffer-file-name buffer-file-name)
+                      (success nil))
+                  (unwind-protect
+                      (progn
+                        (setq buffer-file-name (concat "/sudo::" buffer-file-name))
+                        (save-buffer)
+                        (setq success t))
+                    (setq buffer-file-name old-buffer-file-name)
+                    (when success
+                      (revert-buffer t t))))))))
 
 (provide 'setup-editing)
