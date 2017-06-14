7df5663f40425207fee9e234344f4f62e98250c2
CSRaghunandan <rnraghunandan@gmail.com>
2017-06-14 13:48:24 +0530

    Add xkcd.el package

---
+++ setup-files/setup-xkcd.el
@@ -0,0 +1,36 @@
+;; Time-stamp: <2017-06-14 13:42:32 csraghunandan>
+
+;; xkcd
+;; https://github.com/vibhavp/emacs-xkcd
+(use-package xkcd
+  :defer t
+  :config
+  (progn
+    (bind-keys
+     :map xkcd-mode-map
+     ("/" . xkcd-get)
+     ("g" . xkcd-get-latest)
+     ("c" . xkcd-get-latest-cached)
+     ("p" . xkcd-prev)
+     ("n" . xkcd-next)
+     ("v" . xkcd-open-browser)
+     ("w" . xkcd-copy-link)
+     ("?" . xkcd-open-explanation-browser))))
+
+(provide 'setup-xkcd)
+
+;; |---------+--------------------------------|
+;; | Binding | Description                    |
+;; |---------+--------------------------------|
+;; | /       | Go to a specific comic         |
+;; | g       | Get the latest comic (refresh) |
+;; | c       | Get the latest cached comic    |
+;; | p       | Previous comic                 |
+;; | n       | Next comic                     |
+;; | r       | Random                         |
+;; | v       | Open in browser                |
+;; | w       | Copy link                      |
+;; | ?       | Explain the comic              |
+;; | t       | Show alt text                  |
+;; | q       | Quit                           |
+;; |---------+--------------------------------|
