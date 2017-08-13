129241eb2273d20520b9954114fd9c555f421aa7
CSRaghunandan <rnraghunandan@gmail.com>
2017-08-13 11:37:36 +0530

    Move backup files to tmp folders; add modi/make-backup()

--- setup-files/setup-backup.el
+++ setup-files/setup-backup.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-01-03 17:53:46 csraghunandan>
+;; Time-stamp: <2017-08-13 11:35:43 csraghunandan>
 
 ;; Backup settings for emacs.
 
@@ -17,7 +17,33 @@
 ;; Also backup files which are version controlled
 (setq vc-make-backup-files t)
 
-;; Default and per-save backups go here:
-(setq backup-directory-alist `(("" . ,(concat user-emacs-directory "backup/per-save/"))))
+(setq rag/backup-directory
+      (let ((dir (concat temporary-file-directory
+                         (getenv "USER") "/.backups/"))) ; must end with /
+        (make-directory dir :parents)
+        dir))
+
+;; Save all backups to `rag/backup-directory'
+(setq backup-directory-alist `(("." . ,rag/backup-directory)))
+(message (format "All backup files will be saved to %s." rag/backup-directory))
+
+;; http://ergoemacs.org/emacs/elisp_make-backup.html
+(defun modi/make-backup ()
+  "Make a backup copy of current file.
+The backup file name has the form ‹name›~‹timestamp›~, in the same dir.
+If such a file already exist, it's overwritten.
+If the current buffer is not associated with a file, nothing's done."
+  (interactive)
+  (if (buffer-file-name)
+      (let* ((currentName (buffer-file-name))
+             (backupName (concat currentName
+                                 "." (format-time-string "%Y%m%d_%H%M") ".bkp")))
+        (copy-file currentName backupName :overwrite-if-already-exists)
+        (message (concat "Backup saved as: " (file-name-nondirectory backupName))))
+    (user-error "buffer is not a file.")))
+;; Also make emacs ignore that appended string to the backup files when
+;; deciding the major mode
+;; http://emacs.stackexchange.com/a/13285/115
+(add-to-list 'auto-mode-alist '("\\.[0-9_]+\\.bkp\\'" nil backup-file))
 
 (provide 'setup-backup)
