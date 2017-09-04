926e9e7af54ab71d0a90e36881e444b7385c91e3
CSRaghunandan <rnraghunandan@gmail.com>
2017-09-04 15:14:50 +0530

    improve font-lock for shell scripts

--- setup-files/setup-shell.el
+++ setup-files/setup-shell.el
@@ -2,27 +2,51 @@
 
 ;; configuration for shell mode
 
+(defvar +sh-builtin-keywords
+  '("cat" "cat" "cd" "chmod" "chown" "cp" "curl" "date" "echo" "find" "git"
+    "grep" "head" "kill" "less" "ls" "make" "mkdir" "mv" "pgrep" "pkill" "pwd"
+    "rm" "sleep" "sudo" "tail" "tee" "touch")
+  "A list of common shell commands and keywords to be fontified especially in
+`sh-mode'.")
+
 (use-package sh-script
   :config
-  (defun my-match-variables-in-quotes (limit)
-    "Match variables in double-quotes in `sh-mode'."
+  (defun +sh--match-variables-in-quotes (limit)
+    "Search for variables in double-quoted strings bounded by LIMIT."
     (with-syntax-table sh-mode-syntax-table
-      (catch 'done
-        (while (re-search-forward
-                ;; `rx' is cool, mkay.
-                (rx (group "$")
-                    (group
-                     (or (and "{" (+? nonl) "}")
-                        (and word-start (+? nonl) word-end))))
-                limit t)
-          (-when-let (string-syntax (nth 3 (syntax-ppss)))
-            (when (= string-syntax 34)
-              (throw 'done (point))))))))
+      (let (res)
+        (while
+            (and (setq res
+                       (re-search-forward
+                        "\\(\\$\\)\\({.+?}\\|\\<.+?\\>\\)"
+                        limit t))
+                 (not (eq (nth 3 (syntax-ppss)) ?\"))))
+        res)))
 
+  (defun +sh--match-command-subst-in-quotes (limit)
+    "Search for variables in double-quoted strings bounded by LIMIT."
+    (with-syntax-table sh-mode-syntax-table
+      (let (res)
+        (while
+            (and (setq res
+                       (re-search-forward "\\(\\$(.+?)\\|`.+?`\\)"
+                                          limit t))
+                 (not (eq (nth 3 (syntax-ppss)) ?\"))))
+        res)))
+
+  ;; 1. Fontifies variables in double quotes
+  ;; 2. Fontify command substitution in double quotes
+  ;; 3. Fontify built-in/common commands (see `+sh-builtin-keywords')
   (font-lock-add-keywords
-   'sh-mode '((my-match-variables-in-quotes
-               (1 'default t)
-               (2 font-lock-variable-name-face t)))))
+   'sh-mode `((+sh--match-variables-in-quotes
+               (1 'default prepend)
+               (2 'font-lock-variable-name-face prepend))
+              (+sh--match-command-subst-in-quotes
+               (0 'sh-quoted-exec prepend))
+              (,(regexp-opt +sh-builtin-keywords 'words)
+               (0 'font-lock-builtin-face append))))
+
+  (setq sh-indent-after-continuation 'always))
 
 ;; company-shell: company backend for shell scripts
 ;; https://github.com/Alexander-Miller/company-shell
