4274fec4438b3cc34a6f407a5a95bfcff994ce79
CSRaghunandan <rnraghunandan@gmail.com>
2017-09-18 00:36:02 +0530

    defer some more packages

--- setup-files/setup-config-files.el
+++ setup-files/setup-config-files.el
@@ -2,26 +2,31 @@
 
 ;; yaml-mode: major-mode for editing yaml files
 ;; https://github.com/yoshiki/yaml-mode
-(use-package yaml-mode)
+(use-package yaml-mode
+  :defer t)
 
 ;; conf-mode: major-mdoe for editing conf files
 ;; https://github.com/jrockway/emacs/blob/master/lisp/textmodes/conf-mode.el
 (use-package conf-mode
+  :defer t
   :mode (("\\.conf\\'"    . conf-space-mode)
          ("\\.setup.*\\'" . conf-space-mode)))
 
 ;; systemd: Major mode for editing systemd units
 ;; https://github.com/holomorph/systemd-mode
-(use-package systemd)
+(use-package systemd
+  :defer t)
 
 ;; nginx-mode: Emacs editing mode for Nginx config files
 ;; https://github.com/ajc/nginx-mode
 (use-package nginx-mode
+  :defer t
   :mode (("/nginx/sites-\\(?:available\\|enabled\\)/" . nginx-mode)))
 
 ;; crontab-mode: major-mode for editing corntab files
 ;; https://github.com/emacsorphanage/crontab-mode
 (use-package crontab-mode
+  :defer t
   :mode ("\\.?cron\\(tab\\)?\\'" . crontab-mode))
 
 (provide 'setup-config-files)
--- setup-files/setup-docker.el
+++ setup-files/setup-docker.el
@@ -1,8 +1,9 @@
-;; Time-stamp: <2017-09-10 18:21:20 csraghunandan>
+;; Time-stamp: <2017-09-16 12:13:17 csraghunandan>
 
 ;; dockerfile-mode: An emacs mode for handling Dockerfiles
 ;; https://github.com/spotify/dockerfile-mode
 (use-package dockerfile-mode
+  :defer t
   :mode ("Dockerfile\\'" . dockerfile-mode))
 
 ;; docker: manager docker from emacs
@@ -11,6 +12,7 @@
 
 ;; docker-compose-mode: Major mode for editing docker-compose files
 ;; https://github.com/meqif/docker-compose-mode
-(use-package docker-compose-mode)
+(use-package docker-compose-mode
+  :defer t)
 
 (provide 'setup-docker)
