3c34ac3b8af0a0a36d8680b44a225c89c688182e
CSRaghunandan <rnraghunandan@gmail.com>
2017-09-25 12:54:13 +0530

    use -Wpedantic instead of -pedantic

--- setup-files/setup-cc.el
+++ setup-files/setup-cc.el
@@ -77,7 +77,7 @@
       :config
       (defun +cc|init-c++14-clang-options ()
         (make-local-variable 'irony-additional-clang-options)
-        (cl-pushnew "-std=c++14 -Wall -Wextra -pedantic" irony-additional-clang-options :test 'equal))
+        (cl-pushnew "-std=c++14 -Wall -Wextra -Wpedantic" irony-additional-clang-options :test 'equal))
       (add-hook 'c++-mode-hook #'+cc|init-c++14-clang-options)
 
       (eval-after-load 'flycheck
