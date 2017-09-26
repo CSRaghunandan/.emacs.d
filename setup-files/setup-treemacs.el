a2c2b0314fc6145a125fe319468d17a941cef512
CSRaghunandan <rnraghunandan@gmail.com>
2017-09-26 18:33:41 +0530

    formatting edit: setup-treemacs.el

--- setup-files/setup-treemacs.el
+++ setup-files/setup-treemacs.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-09-25 12:40:54 csraghunandan>
+;; Time-stamp: <2017-09-26 18:32:39 csraghunandan>
 
 ;; treemacs: a tree layout file explorer for Emacs
 ;; https://github.com/Alexander-Miller/treemacs
@@ -7,18 +7,18 @@
   :defer t
   :config
   (progn
-    (setq treemacs-follow-after-init          t
-          treemacs-width                      35
-          treemacs-indentation                2
-          treemacs-git-integration            t
-          treemacs-collapse-dirs              3
-          treemacs-silent-refresh             nil
+    (setq treemacs-follow-after-init t
+          treemacs-width 35
+          treemacs-indentation 2
+          treemacs-git-integration t
+          treemacs-collapse-dirs 3
+          treemacs-silent-refresh nil
           treemacs-change-root-without-asking nil
-          treemacs-sorting                    'alphabetic-desc
-          treemacs-show-hidden-files          t
-          treemacs-never-persist              nil
-          treemacs-is-never-other-window      nil
-          treemacs-goto-tag-strategy          'refetch-index)
+          treemacs-sorting 'alphabetic-desc
+          treemacs-show-hidden-files t
+          treemacs-never-persist nil
+          treemacs-is-never-other-window nil
+          treemacs-goto-tag-strategy 'refetch-index)
 
     (treemacs-follow-mode t)
     (treemacs-filewatch-mode t))
