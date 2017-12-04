2bef965f22b74b4732d711bb8527a052bc3d1f03
CSRaghunandan <rnraghunandan@gmail.com>
2017-12-04 13:54:07 +0530

    Fix swiper keybinding not working in latest use-package update
    revert previous performance commit for ivy as it caused regressions with performance

--- setup-files/setup-swiper.el
+++ setup-files/setup-swiper.el
@@ -1,13 +1,13 @@
-;; Time-stamp: <2017-11-29 20:05:22 csraghunandan>
+;; Time-stamp: <2017-12-04 13:24:12 csraghunandan>
 
 ;; swiper: isearch with an overview!
 ;; https://github.com/abo-abo/swiper
 (use-package swiper
   :bind (:map isearch-mode-map
               ("C-c s" . swiper-from-isearch)) ; isearch > swiper
-  :bind
-  (("C-s" . counsel-grep-or-swiper))
-  :config (setq swiper-action-recenter t))
+  :config
+  (setq swiper-action-recenter t)
+  (bind-key "C-s" #'counsel-grep-or-swiper))
 
 (provide 'setup-swiper)
 
