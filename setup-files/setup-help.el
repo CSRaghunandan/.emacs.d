8d2d9fd9d9c09e03d49c0e440207365d38c3d02d
CSRaghunandan <rnraghunandan@gmail.com>
2018-02-26 11:58:41 +0530

    Bind helpful-* functions to C-? keymap

--- setup-files/setup-help.el
+++ setup-files/setup-help.el
@@ -1,12 +1,15 @@
-;; Time-stamp: <2017-12-02 13:26:34 csraghunandan>
+;; Time-stamp: <2018-02-26 11:57:24 csraghunandan>
 
 ;; helpful: A better Emacs *help* buffer
 ;; https://github.com/Wilfred/helpful
 (use-package helpful
-  :bind
-  (("C-c e c" . helpful-command)
-   ("C-c e f" . helpful-function)
-   ("C-c e m" . helpful-macro)))
+  :bind*
+  (("C-? k" . helpful-key)
+   ("C-? v" . helpful-variable)
+   ("C-? f" . helpful-callable)
+   ("C-? F" . helpful-function)
+   ("C-? C" . helpful-command)
+   ("C-c C-." . helpful-at-point)))
 
 ;; more info in apropos
 (setq apropos-do-all t)
