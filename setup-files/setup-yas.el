38bbec9d81510afd841a91f0cf922453a685a2c1
CSRaghunandan <rnraghunandan@gmail.com>
2018-02-26 13:18:30 +0530

    Add yasnippet-snippets package and don't expand snippets inside comments or strings

--- setup-files/setup-yas.el
+++ setup-files/setup-yas.el
@@ -1,31 +1,36 @@
-;; Time-stamp: <2017-04-09 11:30:08 csraghunandan>
+;; Time-stamp: <2018-02-26 13:04:45 csraghunandan>
+
+;; a collection of yasnippet snippets for many languages
+;; https://github.com/AndreaCrotti/yasnippet-snippets
+(use-package yasnippet-snippets)
 
 ;; yasnippet: snippets tool for emacs
 ;; https://github.com/capitaomorte/yasnippet
 (use-package yasnippet
   :diminish yas-minor-mode
+  :requires yasnippet-snippets
   :config
   (yas-global-mode)
-  (setq yas-triggers-in-field t); Enable nested triggering of snippets
+  (setq yas-triggers-in-field t) ; Enable nested triggering of snippets
   (setq yas-prompt-functions '(yas-completing-prompt))
   (add-hook 'snippet-mode-hook '(lambda () (setq-local require-final-newline nil)))
 
   (bind-key "C-c y"
             (defhydra hydra-yas (:color blue
                                         :hint nil)
-"
+              "
 _i_nsert    _n_ew       _v_isit     aya _c_reate
 _r_eload    e_x_pand    _?_ list    aya _e_xpand
 "
-      ("i" yas-insert-snippet)
-      ("n" yas-new-snippet)
-      ("v" yas-visit-snippet-file)
-      ("r" yas-reload-all)
-      ("x" yas-expand)
-      ("c" aya-create)
-      ("e" aya-expand)
-      ("?" yas-describe-tables)
-      ("q" nil "cancel" :color blue))))
+              ("i" yas-insert-snippet)
+              ("n" yas-new-snippet)
+              ("v" yas-visit-snippet-file)
+              ("r" yas-reload-all)
+              ("x" yas-expand)
+              ("c" aya-create)
+              ("e" aya-expand)
+              ("?" yas-describe-tables)
+              ("q" nil "cancel" :color blue))))
 
 ;; auto-yasnippet: create disposable snippets on the fly
 ;; https://github.com/abo-abo/auto-yasnippet
