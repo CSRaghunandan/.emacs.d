004bc383944f1eb59a36de8564f8275ec691d893
CSRaghunandan <rnraghunandan@gmail.com>
2018-03-13 12:03:43 +0530

    Add elisp-def package
    minor refactor: setup-elisp-mode.el

--- setup-files/setup-elisp-mode.el
+++ setup-files/setup-elisp-mode.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-03-04 21:54:08 csraghunandan>
+;; Time-stamp: <2018-03-13 12:01:17 csraghunandan>
 
 ;; emacs-lisp-mpde
 ;; configure company mode for emacs-lisp-mode
@@ -27,16 +27,24 @@
                           (time-stamp)
                           (xah-clean-whitespace)) nil t)))
 
-  (add-hook 'after-save-hook #'byte-compile-current-buffer)
+  (add-hook 'after-save-hook #'byte-compile-current-buffer))
 
-  ;; highlight-quoted: highlight lisp quoted and quotes symbols
-  ;; https://github.com/Fanael/highlight-quoted
-  (use-package highlight-quoted
-    :hook ((emacs-lisp-mode lisp-mode) . highlight-quoted-mode)
-    :config
-    ;; Highlight the ' character itself in the same colour
-    ;; as the quoted symbol.
-    (set-face-attribute 'highlight-quoted-quote nil
-                        :inherit 'highlight-quoted-symbol)))
+;; highlight-quoted: highlight lisp quoted and quotes symbols
+;; https://github.com/Fanael/highlight-quoted
+(use-package highlight-quoted
+  :hook ((emacs-lisp-mode lisp-mode) . highlight-quoted-mode)
+  :config
+  ;; Highlight the ' character itself in the same colour
+  ;; as the quoted symbol.
+  (set-face-attribute 'highlight-quoted-quote nil
+                      :inherit 'highlight-quoted-symbol))
+
+;; Go to the definition of the symbol at point. Supports global definitions,
+;; local definitions, and even macro-heavy code!
+;; https://github.com/Wilfred/elisp-def
+(use-package elisp-def
+  :bind (:map emacs-lisp-mode-map
+              ("M-." . elisp-def)
+              ("M-," . xref-pop-marker-stack)))
 
 (provide 'setup-elisp-mode)
