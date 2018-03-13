a10ee141bf4e62370760e3df3ab8bcb5d21bce20
CSRaghunandan <rnraghunandan@gmail.com>
2018-03-13 22:55:36 +0530

    Refactor setup-python.el
    replace py-yapf with yapfify; don't run isort on buffer save
    remove pyenv

--- setup-files/setup-python.el
+++ setup-files/setup-python.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-03-12 22:48:06 csraghunandan>
+;; Time-stamp: <2018-03-13 22:48:12 csraghunandan>
 
 ;; Python configuration
 (use-package python
@@ -6,63 +6,15 @@
               (("C-c C-t" . anaconda-mode-show-doc)
                ("M-." . anaconda-mode-find-definitions)
                ("M-," . anaconda-mode-go-back-definitions)))
+  :hook ((python-mode .  (lambda ()
+                           (company-mode)
+                           (smart-dash-mode)
+                           (flycheck-mode)
+                           (setq-local tab-width 4)))
+         (inferior-python-mode . company-mode))
   :config
   ;; don't try to guess python indent offset
   (setq python-indent-guess-indent-offset nil)
-
-  (add-hook 'python-mode-hook (lambda ()
-                                (company-mode)
-                                (smart-dash-mode)
-                                (flycheck-mode)
-                                (setq-local tab-width 4)))
-  ;; enable company-mode completions in inferior python process
-  (add-hook 'inferior-python-mode-hook 'company-mode)
-
-  ;; anaconda-mode: bring IDE like features for python-mode
-  ;; https://github.com/proofit404/anaconda-mode
-  (use-package anaconda-mode
-    :config
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-
-  ;; pyenv: Python virtual environment interface for Emacs
-  ;; https://github.com/jorgenschaefer/pyvenv
-  (use-package pyvenv)
-
-  ;; pyenv-mode: Integrate pyenv with python-mode.
-  ;; https://github.com/proofit404/pyenv-mode
-  (use-package pyenv-mode
-    :if (executable-find "pyenv")
-    :config
-    (add-hook 'python-mode-hook 'pyenv-mode)
-
-    ;; integrate pyenv with projectile
-    (defun projectile-pyenv-mode-set ()
-      "Set pyenv version matching project name."
-      (let ((project (projectile-project-name)))
-        (if (member project (pyenv-mode-versions))
-            (pyenv-mode-set project)
-          (pyenv-mode-unset))))
-
-    (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set))
-
-  ;; company-anaconda: company backend for anaconda
-  ;; https://github.com/proofit404/company-anaconda
-  (use-package company-anaconda
-    :config
-    (defun my-anaconda-mode-hook ()
-      "Hook for `web-mode'."
-      (set (make-local-variable 'company-backends)
-           '((company-anaconda company-files company-yasnippet))))
-    (add-hook 'python-mode-hook 'my-anaconda-mode-hook))
-
-  ;; pytest: for testing python code
-  ;; https://github.com/ionrock/pytest-el
-  (use-package pytest :defer t)
-
-  ;; only install yapfify if yapf is installed
-  (use-package py-yapf
-    :if (executable-find "yapf"))
 
   ;; from https://www.snip2code.com/Snippet/127022/Emacs-auto-remove-unused-import-statemen
   (defun python-remove-unused-imports()
@@ -74,36 +26,68 @@
           (shell-command (format "autoflake --remove-all-unused-imports -i %s"
                                  (shell-quote-argument (buffer-file-name))))
           (revert-buffer t t t))
-      (warn "python-mode: Cannot find autoflake executable, automatic removal of unused imports disabled")))
+      (warn "python-mode: Cannot find autoflake executable, automatic removal of unused imports disabled"))))
 
-  ;; py-isort: sort import statements in python buffers
-  ;; https://github.com/paetzke/py-isort.el
-  (use-package py-isort
-    :if (executable-find "isort")
-    :config
-    (add-hook 'python-mode-hook
-              (lambda ()
-                (add-hook 'before-save-hook
-                          (lambda ()
-                            (time-stamp)
-                            (py-yapf-buffer)
-                            (py-isort-buffer)) nil t))))
+;; pyenv-mode: Integrate pyenv with python-mode.
+;; https://github.com/proofit404/pyenv-mode
+(use-package pyenv-mode
+  :if (executable-find "pyenv")
+  :hook ((python-mode . pyenv-mode))
+  :config
+  ;; integrate pyenv with projectile
+  (defun projectile-pyenv-mode-set ()
+    "Set pyenv version matching project name."
+    (let ((project (projectile-project-name)))
+      (if (member project (pyenv-mode-versions))
+          (pyenv-mode-set project)
+        (pyenv-mode-unset))))
+  (add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set))
 
-  ;; sphinx-doc: add sphinx-doc comments easily
-  ;; https://github.com/naiquevin/sphinx-doc.el
-  (use-package sphinx-doc
-    :config (add-hook 'python-mode-hook 'sphinx-doc-mode))
+;; anaconda-mode: bring IDE like features for python-mode
+;; https://github.com/proofit404/anaconda-mode
+(use-package anaconda-mode
+  :hook ((python-mode . anaconda-mode)
+         (python-mode . anaconda-eldoc-mode)))
 
-  ;; pip-requirements: Major mode for editing pip requirements files
-  ;; https://github.com/Wilfred/pip-requirements.el
-  (use-package pip-requirements
-    :config
-    (add-hook 'pip-requirements-mode 'company-mode))
+;; company-anaconda: company backend for anaconda
+;; https://github.com/proofit404/company-anaconda
+(use-package company-anaconda
+  :after anaconda-mode
+  :config
+  (defun my-anaconda-mode-hook ()
+    "Hook for `web-mode'."
+    (set (make-local-variable 'company-backends)
+         '((company-anaconda company-files company-yasnippet))))
+  (add-hook 'python-mode-hook 'my-anaconda-mode-hook))
 
-  ;; python-docstring: format and highlight syntax for python docstrings
-  ;; https://github.com/glyph/python-docstring-mode
-  (use-package python-docstring
-    :config (add-hook 'python-mode-hook 'python-docstring-mode)))
+;; format python buffers using yapf
+;; https://github.com/JorisE/yapfify/tree/master
+(use-package yapfify
+  :hook ((python-mode . yapf-mode)))
+
+;; py-isort: sort import statements in python buffers
+;; https://github.com/paetzke/py-isort.el
+(use-package py-isort
+  :if (executable-find "isort"))
+
+;; pytest: for testing python code
+;; https://github.com/ionrock/pytest-el
+(use-package pytest :defer t)
+
+;; python-docstring: format and highlight syntax for python docstrings
+;; https://github.com/glyph/python-docstring-mode
+(use-package python-docstring
+  :hook ((python-mode . python-docstring-mode)))
+
+;; pip-requirements: Major mode for editing pip requirements files
+;; https://github.com/Wilfred/pip-requirements.el
+(use-package pip-requirements
+  :hook ((pip-requirements-mode . company-mode)))
+
+;; sphinx-doc: add sphinx-doc comments easily
+;; https://github.com/naiquevin/sphinx-doc.el
+(use-package sphinx-doc
+  :hook ((python-mode . sphinx-doc-mode)))
 
 (provide 'setup-python)
 
