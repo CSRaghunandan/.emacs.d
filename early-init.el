8669bb693d98361ecb54e411011d86afb0258ab8
CSRaghunandan <rnraghunandan@gmail.com>
2018-03-15 15:42:51 +0530

    Add early-init.el file for emacs27

---
+++ early-init.el
@@ -0,0 +1,5 @@
+;; Time-stamp: <2018-03-15 15:40:49 csraghunandan>
+;; Emacs 27.x: http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=24acb31c04b4048b85311d794e600ecd7ce60d3b
+
+(setq package-use-dir "~/.emacs.d/elpa")
+(setq package-enable-at-startup nil)
