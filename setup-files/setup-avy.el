dcced4fe704a4e0f74f35a3bb422e92c57b7e5cb
CSRaghunandan <rnraghunandan@gmail.com>
2018-03-23 17:52:02 +0530

    Remove avy-zap; avy got same functionality in latest commit

--- setup-files/setup-avy.el
+++ setup-files/setup-avy.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-03-05 14:23:05 csraghunandan>
+;; Time-stamp: <2018-03-23 17:51:28 csraghunandan>
 
 ;; avy: package for jumping to visible text using character based decision tree
 ;; https://github.com/abo-abo/avy
@@ -6,17 +6,7 @@
   :bind
   (("C-`" . avy-goto-word-1)
    ("C-'" . avy-goto-char-timer))
-  :config
-
-  ;; use home row keys for avy jumps
-  (setq avy-keys-alist
-        `((avy-goto-char-timer . (?a ?s ?d ?f ?g ?h ?j ?k ?l))))
-  (setq avy-style 'pre))
-
-;; avy-zap: Use avy frontend to yank till the match entered by user
-;; https://github.com/cute-jumper/avy-zap
-(use-package avy-zap
-  :bind (("M-z" . avy-zap-to-char-dwim)))
+  :config (setq avy-style 'pre))
 
 ;; ace-link: quickly traverse through links in info
 ;; https://github.com/abo-abo/ace-link
