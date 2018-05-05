501ee3af2e68760b56f5cfbee3f9fe41be9840ac
CSRaghunandan <rnraghunandan@gmail.com>
2018-05-05 14:41:35 +0530

    refactor typescript configuration

--- setup-files/setup-typescript.el
+++ setup-files/setup-typescript.el
@@ -1,51 +1,18 @@
-;; Time-stamp: <2018-05-05 13:11:53 csraghunandan>
+;; Time-stamp: <2018-05-05 14:39:09 csraghunandan>
 
 ;; typescript config
 
 ;; typescript: major mode for editing typescript files
 ;; https://github.com/ananthakumaran/typescript.el
 (use-package typescript-mode :defer t
+  :hook ((typescript-mode . (lambda ()
+                              (my-tide-setup-hook)
+                              (add-node-modules-path)
+                              (company-mode))))
+  :bind ((:map typescript-mode-map
+               ("C-c C-t" . tide-documentation-at-point)
+               ("C-c T p" . typescript/open-region-in-playground)))
   :config
-
-  ;; tide: TypeScript Interactive Development Environment for Emacs
-  ;; https://github.com/ananthakumaran/tide
-  (use-package tide
-    :config
-    (bind-key "C-c C-t" 'tide-documentation-at-point typescript-mode-map)
-
-    (defun my-tide-setup-hook ()
-      ;; configure tide
-      (tide-setup)
-      ;; highlight identifiers
-      (tide-hl-identifier-mode +1)
-      ;;enable eldoc-mode
-      (eldoc-mode)
-      ;; enable flycheck
-      (flycheck-mode)
-
-      ;; format typescript files using prettier
-      (if (executable-find "prettier")
-          (prettier-js-mode)
-        (warn "typesecript-mode: prettier executable not found, automatic formatting of .ts files are disabled"))
-
-      ;; company-backends setup
-      (set (make-local-variable 'company-backends)
-           '((company-tide company-files company-yasnippet))))
-
-    ;; use 2 space indentation
-    (setq typescript-indent-level 2)
-
-    (add-hook 'typescript-mode-hook #'my-tide-setup-hook)
-    (add-hook 'typescript-mode-hook #'company-mode)
-
-    ;; add tslint checker for flycheck
-    (if (executable-find "tslint")
-        (flycheck-add-next-checker 'typescript-tide
-                                   'typescript-tslint)
-      (warn "typescript-mode: tslint not found. Flycheck support for tslint disabled"))
-
-    (add-hook 'typescript-mode-hook 'add-node-modules-path))
-
   (defun typescript/open-region-in-playground (start end)
     "Open selected region in http://www.typescriptlang.org/Playground
                  If nothing is selected - open the whole current buffer."
@@ -53,7 +20,39 @@
                      (list (region-beginning) (region-end))
                    (list (point-min) (point-max))))
     (browse-url (concat "http://www.typescriptlang.org/Playground#src="
-                        (url-hexify-string (buffer-substring-no-properties start end)))))
-  (bind-key "C-c T p" #'typescript/open-region-in-playground typescript-mode-map))
+                        (url-hexify-string (buffer-substring-no-properties start end))))))
+
+;; tide: TypeScript Interactive Development Environment for Emacs
+;; https://github.com/ananthakumaran/tide
+(use-package tide
+  :after typescript-mode
+  :config
+  (defun my-tide-setup-hook ()
+    ;; configure tide
+    (tide-setup)
+    ;; highlight identifiers
+    (tide-hl-identifier-mode +1)
+    ;;enable eldoc-mode
+    (eldoc-mode)
+    ;; enable flycheck
+    (flycheck-mode)
+
+    ;; format typescript files using prettier
+    (if (executable-find "prettier")
+        (prettier-js-mode)
+      (warn "typesecript-mode: prettier executable not found, automatic formatting of .ts files are disabled"))
+
+    ;; company-backends setup
+    (set (make-local-variable 'company-backends)
+         '((company-tide company-files company-yasnippet))))
+
+  ;; use 2 space indentation
+  (setq typescript-indent-level 2)
+
+  ;; add tslint checker for flycheck
+  (if (executable-find "tslint")
+      (flycheck-add-next-checker 'typescript-tide
+                                 'typescript-tslint)
+    (warn "typescript-mode: tslint not found. Flycheck support for tslint disabled")))
 
 (provide 'setup-typescript)
