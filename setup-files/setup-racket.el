b73396c74800c19acbf2ab9ce229252cb9606e7b
CSRaghunandan <rnraghunandan@gmail.com>
2018-05-05 18:21:39 +0530

    Refactor racket-mode configuration to use :hook

--- setup-files/setup-racket.el
+++ setup-files/setup-racket.el
@@ -1,18 +1,17 @@
-;; Time-stamp: <2017-09-10 14:41:11 csraghunandan>
+;; Time-stamp: <2018-05-05 18:16:29 csraghunandan>
 
 ;; racket-mode: major mode for editing racket files
 ;; https://github.com/greghendershott/racket-mode
 (use-package racket-mode :defer t
+  :hook ((racket-mode . (lambda ()
+                          (my-racket-mode-hook)
+                          (company-mode)))
+         (racket-repl-mode . (lambda ()
+                               (my-racket-mode-hook)
+                               (company-mode))))
   :config
   (defun my-racket-mode-hook ()
     (set (make-local-variable 'company-backends)
-         '((company-capf company-dabbrev-code company-files company-yasnippet))))
-  (defun my-racket-mode-hook2 ()
-    (set (make-local-variable 'company-backends)
-         '((company-capf company-dabbrev-code company-yasnippet))))
-  (add-hook 'racket-mode-hook 'my-racket-mode-hook)
-  (add-hook 'racket-mode-hook 'company-mode)
-  (add-hook 'racket-repl-mode-hook 'my-racket-mode-hook2)
-  (add-hook 'racket-repl-mode-hook 'company-mode))
+         '((company-capf company-dabbrev-code company-files company-yasnippet)))))
 
 (provide 'setup-racket)
