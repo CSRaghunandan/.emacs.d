cbbf32391d6f4cb3e349eacb7ab4301b590be61b
CSRaghunandan <rnraghunandan@gmail.com>
2018-05-10 16:51:06 +0530

    Improve pdf-tools package

--- setup-files/setup-pdf.el
+++ setup-files/setup-pdf.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-05-07 17:24:58 csraghunandan>
+;; Time-stamp: <2018-05-10 16:50:04 csraghunandan>
 
 ;; pdf-tools: Emacs support library for PDF files.
 ;; https://github.com/politza/pdf-tools
@@ -7,7 +7,27 @@
   ;; https://github.com/politza/pdf-tools/issues/312#issuecomment-329537742
   ;; Build the program (if necessary) without asking first, if NO-QUERY-P is
   ;; non-nil.
-  (pdf-tools-install))
+  (pdf-tools-install)
+
+  ;; automatically annotate highlights
+  (setq pdf-annot-activate-created-annotations t)
+  ;; use isearch instead of swiper
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  ;; turn off cua so copy works
+  (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
+  ;; more fine-grained zooming
+  (setq pdf-view-resize-factor 1.1)
+
+  ;; keyboard shortcuts
+  (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
+  (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
+  (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete)
+  ;; wait until map is available
+  (with-eval-after-load "pdf-annot"
+    (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<return>") 'pdf-annot-edit-contents-commit)
+    (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<S-return>") 'newline)
+    ;; save after adding comment
+    (advice-add 'pdf-annot-edit-contents-commit :after 'bjm/save-buffer-no-args)))
 
 (provide 'setup-pdf)
 
