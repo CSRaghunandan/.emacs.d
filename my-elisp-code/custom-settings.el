f0cb9e6812c856d25686b14f740b6486d4faa5a1
CSRaghunandan <rnraghunandan@gmail.com>
2018-05-11 10:45:57 +0530

    Add configuration for whitespace-mode
    Set sensible whitespace-style variable

--- my-elisp-code/custom-settings.el
+++ my-elisp-code/custom-settings.el
@@ -38,4 +38,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#383838")))))
+ )
--- setup-files/setup-white-space.el
+++ setup-files/setup-white-space.el
@@ -1,8 +1,26 @@
-;; Time-stamp: <2018-03-13 10:18:29 csraghunandan>
+;; Time-stamp: <2018-05-11 10:44:56 csraghunandan>
 
 ;; ws-butler: clean trailing whitespaces unobtrusively
 ;; https://github.com/lewang/ws-butler
 (use-package ws-butler
   :hook ((prog-mode . ws-butler-mode)))
+
+;;; Whitespace Mode/Show Long Lines
+(use-package whitespace :defer t
+  :ensure nil
+  :config
+  (setq whitespace-line-column nil)  ;When nil, set the value to `fill-column'
+  (setq whitespace-style
+        '(face
+          trailing                    ;White space at end of lines
+          tabs                        ;tab-mark ;`tab-mark' shows tabs as '»'
+          spaces space-mark           ;`space-mark' shows spaces as '.'
+          space-before-tab space-after-tab ;Mix of tabs and spaces
+          ;; lines   ;highlight lines that extend beyond `whitespace-line-column'
+          lines-tail ;highlight only characters beyond `whitespace-line-column'
+          ;; newline newline-mark
+          ;; empty ;blank lines at BOB or EOB
+          indentation)) ;highlight spaces/tabs at BOL depending on `indent-tabs-mode'
+  )
 
 (provide 'setup-white-space)
