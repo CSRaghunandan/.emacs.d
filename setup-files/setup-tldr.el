323bb33972fa4cedf64fb1b1d1e39857a33a8c5a
CSRaghunandan <rnraghunandan@gmail.com>
2018-06-08 14:02:51 +0530

    No need to ensure tldr package

--- setup-files/setup-tldr.el
+++ setup-files/setup-tldr.el
@@ -1,9 +1,9 @@
-;; Time-stamp: <2017-06-14 13:37:23 csraghunandan>
+;; Time-stamp: <2018-06-07 23:20:43 csraghunandan>
 
 ;; TLDR
 ;; https://github.com/tldr-pages/tldr
 ;; https://github.com/kuanyui/tldr.el
-(use-package tldr :ensure t
+(use-package tldr
   :bind (("C-x / t" . tldr)))
 
 ;; Related -- curl cheat.sh
