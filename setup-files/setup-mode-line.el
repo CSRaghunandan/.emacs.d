a8c572ec1b4b77e8cac872acb7144c3d55903e16
CSRaghunandan <rnraghunandan@gmail.com>
2018-06-09 17:06:12 +0530

    Add documentation for display-time-format

--- setup-files/setup-mode-line.el
+++ setup-files/setup-mode-line.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-06-09 17:04:15 csraghunandan>
+;; Time-stamp: <2018-06-09 17:06:07 csraghunandan>
 
 ;; flash the modeline instead of ringing the bell
 ;; https://github.com/purcell/mode-line-bell
@@ -80,3 +80,21 @@
 (rename-modeline "haskell-mode" haskell-mode "𝞴=")
 
 (provide 'setup-mode-line)
+
+;; Variables used in display-time-format
+;; http://docs.splunk.com/Documentation/Splunk/5.0.2/SearchReference/Commontimeformatvariables
+;;
+;; | %y | year in numbers (2-digit)                   |
+;; | %Y | year in numbers (4-digit)                   |
+;; | %m | month in number (eg: 12)                    |
+;; | %B | full month name (eg: December)              |
+;; | %b | short month name (eg: Dec)                  |
+;; | %d | day in numbers, with leading zeros (eg: 08) |
+;; | %e | day in numbers, no leading zeros (eg: 8)    |
+;; | %A | full weekday name (eg: Sunday)              |
+;; | %a | short weekday name (eg: Sun)                |
+;; | %H | hours in 24-clock, with leading zeros       |
+;; | %k | hours in 24-clock, no leading zeros         |
+;; | %l | hours in 12-clock, with leading zeros       |
+;; | %p | am/pm                                       |
+;; | %T | time in 24-hour notation (%H:%M:%S)         |
