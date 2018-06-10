cb761b1c09f7d3747eebc45121dd4b36e78bdb36
CSRaghunandan <rnraghunandan@gmail.com>
2018-06-11 02:31:02 +0530

    Minor changes to comint-mode configuration

--- setup-files/setup-comint.el
+++ setup-files/setup-comint.el
@@ -1,27 +1,21 @@
-;; Time-stamp: <2017-02-12 13:30:07 csraghunandan>
+;; Time-stamp: <2018-06-11 02:30:01 csraghunandan>
 
 ;; comint: all inferior processes inherit from comint-mode
 (use-package comint :ensure nil
+  :bind
+  (:map comint-mode-map
+        ("C-l" . my-recenter-top-bottom)
+        ("<up>" . comint-previous-input)
+        ("<down>" . comint-next-input ))
+  :custom
+  (comint-scroll-to-bottom-on-input t)
+  (comint-process-echoes t "prevent comint process from echoing the command typed to the user")
   :config
-  ;; always insert input at bottom
-  (setq comint-scroll-to-bottom-on-input t)
-
   ;; makes sense to not recenter to the middle for comint buffers. Only top/bottom
   (defun my-recenter-top-bottom ()
     (interactive)
     (goto-char (point-max))
     (let ((recenter-positions '(top bottom)))
-      (recenter-top-bottom)))
-
-  (bind-key "C-l" 'my-recenter-top-bottom comint-mode-map)
-
-  ;; prevent comint process from echoing the command typed to the user
-  (setq-default comint-process-echoes t)
-
-  ;; remap up and down to previous and next commands in history
-  (bind-keys
-   :map comint-mode-map
-   ("<up>" . comint-previous-input)
-   ("<down>" . comint-next-input)))
+      (recenter-top-bottom))))
 
 (provide 'setup-comint)
