b714795b85ee520e0caa33bee4b58d207f674f2f
CSRaghunandan <rnraghunandan@gmail.com>
2018-06-11 01:43:32 +0530

    Minor changes to ace-window configuration

--- setup-files/setup-ace-window.el
+++ setup-files/setup-ace-window.el
@@ -1,16 +1,27 @@
-;; Time-stamp: <2018-03-13 00:44:16 csraghunandan>
+;; Time-stamp: <2018-06-11 01:43:18 csraghunandan>
 
 ;; ace-window: quick switching of windows
 ;; https://github.com/abo-abo/ace-window
 (use-package ace-window
   :commands (ace-window)
   :bind* ("C-c w" . ace-window)
+  :custom
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (aw-dispatch-always t "enable aw dispatch even for just one window")
+  ;; add hydras to control window size and scroll other window
+  (aw-dispatch-alist
+   '((?x aw-delete-window " Ace - Delete Window")
+     (?m aw-swap-window " Ace - Swap Window")
+     (?n aw-flip-window)
+     (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+     (?j aw-switch-buffer-in-window "Select Buffer")
+     (?o delete-other-windows " Ace - Maximize Window")
+     (?c hydra-window-scroll/body)
+     (?t aw-split-window-fair "Ace- split fairly")
+     (?y hydra-window-size/body)
+     (?? aw-show-dispatch-help)))
+
   :config
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-
-  ;; enable aw dispatch even for just one window
-  (setq aw-dispatch-always t)
-
   (defhydra hydra-window-size (:color red)
     "Windows size"
     ("b" shrink-window-horizontally "shrink horizontal")
@@ -23,23 +34,9 @@
 
   (defhydra hydra-window-scroll (:color red)
     "Scroll other window"
-    ("n" scroll-other-window "scroll")
-    ("p" scroll-other-window-down "scroll down")
-    ("q" nil :color blue))
-
-  ;; add hydras to control window size and scroll other window
-  (setq aw-dispatch-alist
-    '((?x aw-delete-window " Ace - Delete Window")
-      (?m aw-swap-window " Ace - Swap Window")
-      (?M aw-move-window "Move Window")
-      (?n aw-flip-window)
-      (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
-      (?j aw-switch-buffer-in-window "Select Buffer")
-      (?o delete-other-windows " Ace - Maximize Window")
-      (?c hydra-window-scroll/body)
-      (?t aw-split-window-fair "Ace- split fairly")
-      (?y hydra-window-size/body)
-      (?? aw-show-dispatch-help))))
+    ("<SPC>" scroll-other-window "scroll")
+    ("b" scroll-other-window-down "scroll down")
+    ("q" nil :color blue)))
 
 (provide 'setup-ace-window)
 
@@ -52,7 +49,6 @@
 ;; x - delete window
 ;; m - swap (move) window
 ;; t - split window fairly
-;; M - move window
 ;; u - switch buffer other window
 ;; j - switch buffer in window
 ;; n - select the previous window
