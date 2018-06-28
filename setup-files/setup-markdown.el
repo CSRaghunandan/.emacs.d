5c91755e6c7ca7b42bef1dbd1ee90a908244ce97
CSRaghunandan <rnraghunandan@gmail.com>
2018-06-28 16:36:15 +0530

    Do not hide markdown markups
    Editing markdown files is better with markups enabled

--- setup-files/setup-markdown.el
+++ setup-files/setup-markdown.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-06-28 12:45:13 csraghunandan>
+;; Time-stamp: <2018-06-28 16:29:52 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghuandan rnraghunandan@gmail.com
@@ -18,7 +18,6 @@
 
   (setq markdown-fontify-code-blocks-natively t)
   (setq markdown-list-indent-width 2)
-  (setq-default markdown-hide-markup t)
 
   ;; Don't change font in code blocks
   (set-face-attribute 'markdown-code-face nil
