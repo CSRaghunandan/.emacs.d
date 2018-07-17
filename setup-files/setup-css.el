a6be97ee4f82e0c5e17bae9ef6ef0e06ce6539ee
CSRaghunandan <rnraghunandan@gmail.com>
2018-07-17 13:37:16 +0530

    Enable lsp-css for scss, sass and less files as well

--- setup-files/setup-css.el
+++ setup-files/setup-css.el
@@ -1,35 +1,37 @@
-;; Time-stamp: <2018-07-17 13:26:28 csraghunandan>
+;; Time-stamp: <2018-07-17 13:34:51 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
+
+(defun my-css-mode-hook ()
+  (set (make-local-variable 'company-backends)
+       '((company-lsp company-files :with company-yasnippet)
+         (company-dabbrev-code company-dabbrev))))
+
+(defun lsp-css-common-setup()
+  (lsp-ui-mode)
+  (eldoc-mode)
+  (flycheck-mode)
+  (my-css-mode-hook)
+  (company-mode)
+  (emmet-mode)
+  (prettier-js-mode)
+  (rainbow-mode))
 
 ;; css-mode config
 (use-package css-mode
   :ensure nil
   :hook ((css-mode . (lambda ()
-                       (rainbow-mode)
-                       (lsp-css-mode-setup)
-                       (my-css-mode-hook)
-                       (company-mode)
-                       (emmet-mode)
-                       (prettier-js-mode))))
+                       (my-css-mode-setup)
+                       (lsp-css-common-setup))))
   :config
-
-  (defun lsp-css-mode-setup ()
+  (defun my-css-mode-setup ()
     (when (eq major-mode 'css-mode)
       ;; Only enable in strictly css-mode, not scss-mode (css-mode-hook
       ;; fires for scss-mode because scss-mode is derived from css-mode)
-      (lsp-css-enable)
-      (lsp-ui-mode)
-      (eldoc-mode)
-      (flycheck-mode)))
+      (lsp-css-enable)))
 
-  (setq css-indent-offset 2)
-
-  (defun my-css-mode-hook ()
-    (set (make-local-variable 'company-backends)
-         '((company-lsp company-files :with company-yasnippet)
-           (company-dabbrev-code company-dabbrev)))))
+  (setq css-indent-offset 2))
 
 ;; CSS, LESS, and SCSS/SASS support for lsp-mode using vscode-css-languageserver-bin
 ;; https://github.com/emacs-lsp/lsp-css
@@ -38,15 +40,24 @@
 
 (use-package less-css-mode              ; Mode for Less CSS files
   :ensure nil
-  :mode "\\.less\\'")
+  :mode "\\.less\\'"
+  :hook ((less-css . (lambda ()
+                       (lsp-less-enable)
+                       (lsp-css-common-setup)))))
 
 ;; major mode for editing sass files
 ;; https://github.com/nex3/sass-mode
 (use-package sass-mode
-  :mode (("\\.sass\\'" . sass-mode)))
+  :mode (("\\.sass\\'" . sass-mode))
+  :hook ((sass-mode . (lambda ()
+                        (lsp-scss-enable)
+                        (lsp-css-common-setup)))))
 
 (use-package scss-mode                  ; Mode for SCSS files
   :ensure nil
-  :mode "\\.scss\\'")
+  :mode "\\.scss\\'"
+  :hook ((sass-mode . (lambda ()
+                        (lsp-scss-enable)
+                        (lsp-css-common-setup)))))
 
 (provide 'setup-css)
