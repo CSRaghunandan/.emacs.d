5846bf48f2fa7c7cc2ff857e7fe80e510b6ae417
CSRaghunandan <rnraghunandan@gmail.com>
2018-08-15 03:23:17 +0530

    lazy load exec-path-from-shell

--- setup-files/setup-osx.el
+++ setup-files/setup-osx.el
@@ -1,5 +1,5 @@
 ;;; setup-osx.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 03:05:30 csraghunandan>
+;; Time-stamp: <2018-08-15 03:16:15 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -8,16 +8,41 @@
 
 (when (is-mac-p)
   (setq ns-function-modifier 'hyper ; make Fn key do Hyper
-        ns-use-thin-smoothing t))
+        ns-use-thin-smoothing t
+        ;; sane trackpad/mouse scroll settings
+        mac-redisplay-dont-reset-vscroll t
+        mac-mouse-wheel-smooth-scroll nil
+        mouse-wheel-scroll-amount '(5 ((shift) . 2))  ; one line at a time
+        mouse-wheel-progressive-speed nil             ; don't accelerate scrolling
+        ;; Curse Lion and its sudden but inevitable fullscreen mode!
+        ;; NOTE Meaningless to railwaycat's emacs-mac build
+        ns-use-native-fullscreen nil
+        ;; Don't open files from the workspace in a new frame
+        ns-pop-up-frames nil))
 
 ;; exec-path-from-shell: ensure environment variables inside Emacs look the same
 ;; as in the users shell
 ;; https://github.com/purcell/exec-path-from-shell
 (use-package exec-path-from-shell
-  :if (is-mac-p)
-  :init
-  (setq exec-path-from-shell-check-startup-files nil)
-  (exec-path-from-shell-initialize))
+  :if (is-mac-p))
+
+(cond ((display-graphic-p)
+       ;; A known problem with GUI Emacs on MacOS: it runs in an isolated
+       ;; environment, so envvars will be wrong. That includes the PATH
+       ;; Emacs picks up. `exec-path-from-shell' fixes this. This is slow
+       ;; and benefits greatly from compilation.
+       (setq exec-path
+             (or (eval-when-compile
+                   (when (require 'exec-path-from-shell nil t)
+                     (setq exec-path-from-shell-check-startup-files nil
+                           exec-path-from-shell-arguments (delete "-i" exec-path-from-shell-arguments))
+                     (nconc exec-path-from-shell-variables '("GOPATH" "GOROOT" "PYTHONPATH"))
+                     (exec-path-from-shell-initialize)
+                     exec-path))
+                 exec-path)))
+      (t
+       (when (require 'osx-clipboard nil t)
+         (osx-clipboard-mode +1))))
 
 ;; delete files by moving to trash in macOS
 ;; https://github.com/lunaryorn/osx-trash.el
