5ce5a33bcc9fb860d3b928a896b7b7ef71e45e1e
CSRaghunandan <rnraghunandan@gmail.com>
2018-12-13 21:57:12 +0530

    Update ccls configuration based on latest code from MaskRay github

--- setup-files/setup-cc.el
+++ setup-files/setup-cc.el
@@ -1,5 +1,5 @@
 ;;; setup-cc.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-10-02 23:46:02 csraghunandan>
+;; Time-stamp: <2018-12-13 21:54:50 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -35,10 +35,11 @@
    ccls-extra-init-params
    `(:clang (:pathMappings ,+ccls-path-mappings)
             :completion
-            (:includeBlacklist
-             ("^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
-              "^/usr/(local/)?include/c\\+\\+/v1/"
-              ))
+            (:include
+             (:blacklist
+              ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
+               "^/usr/(local/)?include/c\\+\\+/v1/"
+               ]))
             :index (:initialBlacklist ,+ccls-initial-blacklist :trackDependency 1)))
   :config
   ;; enable ccls semantic highlighting
@@ -50,59 +51,51 @@
   ;; https://github.com/MaskRay/Config/blob/master/home/.config/doom/modules/private/my-cc/autoload.el#L10
   (defun ccls/callee ()
     (interactive)
-    (lsp-ui-peek-find-custom 'callee "$ccls/call" '(:callee t)))
+    (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
   (defun ccls/caller ()
     (interactive)
-    (lsp-ui-peek-find-custom 'caller "$ccls/call"))
+    (lsp-ui-peek-find-custom "$ccls/call"))
   (defun ccls/vars (kind)
-    (lsp-ui-peek-find-custom 'vars "$ccls/vars" `(:kind ,kind)))
+    (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
   (defun ccls/base (levels)
-    (lsp-ui-peek-find-custom 'base "$ccls/inheritance" `(:levels ,levels)))
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
   (defun ccls/derived (levels)
-    (lsp-ui-peek-find-custom 'derived "$ccls/inheritance" `(:levels ,levels :derived t)))
+    (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
   (defun ccls/member (kind)
-    (lsp-ui-peek-find-custom 'member "$ccls/member" `(:kind ,kind)))
-  ;; The meaning of :role corresponds to https://github.com/maskray/ccls/blob/master/src/symbol.h
+    (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
 
+  ;; The meaning of :role corresponds to https://github.com/maskray/ccls/blob/master/src/symbol.h
   ;; References w/ Role::Address bit (e.g. variables explicitly being taken addresses)
   (defun ccls/references-address ()
     (interactive)
-    (lsp-ui-peek-find-custom
-     'address "textDocument/references"
-     (plist-put (lsp--text-document-position-params) :context
-                '(:role 128))))
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 128)))
 
   ;; References w/ Role::Dynamic bit (macro expansions)
   (defun ccls/references-macro ()
     (interactive)
-    (lsp-ui-peek-find-custom
-     'address "textDocument/references"
-     (plist-put (lsp--text-document-position-params) :context
-                '(:role 64))))
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 64)))
 
   ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
   (defun ccls/references-not-call ()
     (interactive)
-    (lsp-ui-peek-find-custom
-     'address "textDocument/references"
-     (plist-put (lsp--text-document-position-params) :context
-                '(:excludeRole 32))))
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :excludeRole 32)))
 
   ;; References w/ Role::Read
   (defun ccls/references-read ()
     (interactive)
-    (lsp-ui-peek-find-custom
-     'read "textDocument/references"
-     (plist-put (lsp--text-document-position-params) :context
-                '(:role 8))))
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 8)))
 
   ;; References w/ Role::Write
   (defun ccls/references-write ()
     (interactive)
-    (lsp-ui-peek-find-custom
-     'write "textDocument/references"
-     (plist-put (lsp--text-document-position-params) :context
-                '(:role 16)))))
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 16)))
+
+  (add-hook 'lsp-after-open-hook #'ccls-code-lens-mode))
 
 (defun ccls//enable ()
   "Enable lsp-ccls"
