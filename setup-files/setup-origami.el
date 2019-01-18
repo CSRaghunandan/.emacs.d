844e40a97e384203b762b55f156f03f8f48d5c97
CSRaghunandan <rnraghunandan@gmail.com>
2019-01-18 16:37:53 +0530

    refactor setup-origami

--- setup-files/setup-origami.el
+++ setup-files/setup-origami.el
@@ -1,5 +1,5 @@
 ;;; setup-origami.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 03:05:20 csraghunandan>
+;; Time-stamp: <2019-01-18 16:37:45 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -7,23 +7,23 @@
 ;; origami: better folding package than hs-minor-mode
 ;; https://github.com/gregsexton/origami.el
 (use-package origami
+  :bind ("C-c h o" . hydra-origami/body)
   :config
-  (global-origami-mode)
-
-  (bind-key "C-c h o"
-            (defhydra hydra-origami (:color red
-                                            :hint nil)
-"
+  (defhydra hydra-origami (:color red
+                                  :hint nil)
+    "
 _t_: toggle    _r_: redo    _p_: prev        _c_: close all
 _u_: undo      _n_: next    _o_: open all    _q_: quit
 "
-              ("t" origami-recursively-toggle-node)
-              ("u" origami-undo)
-              ("r" origami-redo)
-              ("p" origami-previous-fold)
-              ("n" origami-next-fold)
-              ("o" origami-open-all-nodes)
-              ("c" origami-close-all-nodes)
-              ("q" nil :color blue))))
+    ("t" origami-recursively-toggle-node)
+    ("u" origami-undo)
+    ("r" origami-redo)
+    ("p" origami-previous-fold)
+    ("n" origami-next-fold)
+    ("o" origami-open-all-nodes)
+    ("c" origami-close-all-nodes)
+    ("q" nil :color blue))
+
+  (global-origami-mode))
 
 (provide 'setup-origami)
