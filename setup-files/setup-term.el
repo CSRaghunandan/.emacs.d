8261a695ef80eae7aeb37ebae6265fae92a74bf3
CSRaghunandan <rnraghunandan@gmail.com>
2020-01-22 14:50:10 +0530

    Add: vterm configuration for emacs

--- setup-files/setup-term.el
+++ setup-files/setup-term.el
@@ -1,83 +1,51 @@
 ;;; setup-term.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-12-14 01:51:13 csraghunandan>
+;; Time-stamp: <2020-01-22 14:48:20 csraghunandan>
 
 ;; Copyright (C) 2016-2018 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
 
-;; multi-term: manage multiple terminal windows easily within emacs
-;; https://github.com/emacsorphanage/multi-term/tree/f954e4e18b0a035151d34852387e724d87a3316f
-(use-package multi-term
+;; vterm: Emacs libvterm integration
+;; https://github.com/akermu/emacs-libvterm
+(use-package vterm
   :config
-  ;; Some userlike the cursor return to the position it was before I opened the
-  ;; dedicated terminal window.
-  (setq multi-term-dedicated-close-back-to-open-buffer-p t)
 
-  (defun last-term-buffer (l)
-    "Return most recently used term buffer."
-    (when l
-      (if (eq 'term-mode (with-current-buffer (car l) major-mode))
-          (car l) (last-term-buffer (cdr l)))))
+  ;; disable some unnecessary minor-modes in term-mode
+  (add-hook 'vterm-mode-hook (lambda ()
+                               (yas-minor-mode -1)
+                               (whole-line-or-region-local-mode -1)
+                               (setq-local global-hl-line-mode nil)
+                               (hungry-delete-mode -1)
 
-  (defun get-term ()
-    "Switch to the term buffer last used, or create a new one if
-    none exists, or if the current buffer is already a term."
-    (interactive)
-    (let ((b (last-term-buffer (buffer-list))))
-      (if (or (not b) (eq 'term-mode major-mode))
-          (multi-term)
-        (switch-to-buffer b))))
+                               ;; Don't prompt about processes when killing vterm
+                               (setq-local confirm-kill-processes nil)
+                               ;; Prevent premature horizontal scrolling
+                               (setq-local hscroll-margin 0)
+                               )))
 
+;; vterm-toggle: toggles between the vterm buffer and whatever buffer you are editing.
+;; https://github.com/jixiuf/vterm-toggle
+(use-package vterm-toggle)
+
+;; multi-libvterm: manage multiple terminal windows easily within emacs
+;; https://github.com/suonlight/multi-libvterm
+(use-package multi-libvterm
+  :load-path "~/.emacs.d/elisp/multi-libvterm"
+  :config
   (bind-key "C-c t"
             (defhydra multi-term-hydra ()
               "multi-term"
-              ("o" get-term "toggle/open")
-              ("n" multi-term-next "Next")
-              ("p" multi-term-prev "Prev")
-              ("d" multi-term-dedicated-toggle "Dedicated terminal")
+              ("o" multi-libvterm "new terminal")
+              ("t" vterm-toggle-cd "toggle/open")
+              ("n" multi-libvterm-next "Next")
+              ("p" multi-libvterm-prev "Prev")
+              ("d" multi-libvterm-dedicated-toggle "Dedicated terminal")
+              ("r" multi-libvterm-projectile "vterm projectile")
               ("q" nil "Quit" :color blue))))
 
 ;; eterm-256color: Customizable 256 colors for emacs term and ansi-term
 ;; https://github.com/dieggsy/eterm-256color
 (use-package eterm-256color :defer t
-  :hook ((term-mode . eterm-256color-mode)))
-
-(use-package term :ensure nil
-  :config
-  ;; bind-keys for term-mode
-  (setq term-bind-key-alist
-        '(("C-c C-c" . term-interrupt-subjob)
-          ("C-v" . scroll-up)
-          ("M-v" . scroll-down)
-          ("C-c C-e" . term-send-esc)
-          ("C-c C-j" . term-line-mode)
-          ("C-c C-k" . term-char-mode)
-          ("C-b"     . term-send-left)
-          ("C-f"     . term-send-right)
-          ("C-p"     . previous-line)
-          ("C-n"     . next-line)
-          ("C-s"     . swiper)
-          ("C-m"     . term-send-return)
-          ("C-y"     . term-paste)
-          ("M-f"     . term-send-forward-word)
-          ("M-b"     . term-send-backward-word)
-          ("M-p"     . term-send-up)
-          ("M-n"     . term-send-down)
-          ("M-d"     . term-send-forward-kill-word)
-          ("C-M-h"   . term-send-backward-kill-word)
-          ("M-r"     . term-send-reverse-search-history)
-          ("M-,"     . term-send-raw)
-          ("M-." . comint-dynamic-complete)))
-
-  ;; disable some unnecessary minor-modes in term-mode
-  (add-hook 'term-mode-hook (lambda ()
-                              (yas-minor-mode -1)
-                              (whole-line-or-region-local-mode -1)
-                              (setq-local global-hl-line-mode nil)
-                              (hungry-delete-mode -1)))
-
-  (setq multi-term-buffer-name "term")
-  (setq multi-term-program (getenv "SHELL")
-        multi-term-switch-after-close 'PREVIOUS))
+  :hook ((vterm-mode . eterm-256color-mode)))
 
 (provide 'setup-term)
 
