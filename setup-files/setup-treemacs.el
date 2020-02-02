1db42e4909dd737213e43f3b2b15ac168754f06d
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-02 21:46:06 +0530

    Fix: disable treemacs-icons-dired until issue with doom-emacs integration is fixed
    https://github.com/hlissner/emacs-doom-themes/issues/388

--- setup-files/setup-treemacs.el
+++ setup-files/setup-treemacs.el
@@ -1,5 +1,5 @@
 ;;; setup-treemacs.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-01-23 12:02:38 csraghunandan>
+;; Time-stamp: <2020-02-02 21:41:42 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -56,6 +56,7 @@
 ;; https://github.com/Alexander-Miller/treemacs/blob/master/src/extra/treemacs-icons-dired.el
 (use-package treemacs-icons-dired
   :after treemacs dired
+  :disabled t ;; disable till integration with doom-themes treemacs config is fixed
   :config (treemacs-icons-dired-mode))
 
 (provide 'setup-treemacs)
