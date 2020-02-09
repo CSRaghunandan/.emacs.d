4f422598f1bc548804c9ce5bfa94fd20948cdf88
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-10 00:08:17 +0530

    Cut: remove lot of unnecessary code in backup config
    Add: enable auto-save-mode

--- setup-files/setup-backup.el
+++ setup-files/setup-backup.el
@@ -1,13 +1,10 @@
 ;;; setup-backup.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 02:40:35 csraghunandan>
+;; Time-stamp: <2020-02-10 00:07:14 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
 
 ;; Backup settings for emacs.
-
-;; disable auto safe
-(setq auto-save-default nil)
 
 ;; No need for ~ files when editing
 (setq create-lockfiles nil)
@@ -21,30 +18,6 @@
 ;; Also backup files which are version controlled
 (setq vc-make-backup-files t)
 
-(setq rag/backup-directory
-      (let ((dir (concat temporary-file-directory
-                         (getenv "USER") "/.backups/"))) ; must end with /
-        (make-directory dir :parents)
-        dir))
-
-;; Save all backups to `rag/backup-directory'
-(setq backup-directory-alist `(("." . ,rag/backup-directory)))
-(message (format "All backup files will be saved to %s." rag/backup-directory))
-
-;; http://ergoemacs.org/emacs/elisp_make-backup.html
-(defun modi/make-backup ()
-  "Make a backup copy of current file.
-The backup file name has the form ‹name›~‹timestamp›~, in the same dir.
-If such a file already exist, it's overwritten.
-If the current buffer is not associated with a file, nothing's done."
-  (interactive)
-  (if (buffer-file-name)
-      (let* ((currentName (buffer-file-name))
-             (backupName (concat currentName
-                                 "." (format-time-string "%Y%m%d_%H%M") ".bkp")))
-        (copy-file currentName backupName :overwrite-if-already-exists)
-        (message (concat "Backup saved as: " (file-name-nondirectory backupName))))
-    (user-error "buffer is not a file.")))
 ;; Also make emacs ignore that appended string to the backup files when
 ;; deciding the major mode
 ;; http://emacs.stackexchange.com/a/13285/115
