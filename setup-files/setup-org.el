ce7679d4aead7b79c109d03bc87a9f494aba7b2a
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-10 19:17:23 +0530

    Cut: remove code from org config file
    No longer revelant to my usage. I want to keep modifications to a minimum

--- setup-files/setup-org.el
+++ setup-files/setup-org.el
@@ -1,5 +1,5 @@
 ;;; setup-org.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-02-10 18:52:14 csraghunandan>
+;; Time-stamp: <2020-02-10 19:13:34 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -553,122 +553,6 @@ text and copying to the killring."
     ("m" org-timer)
     ("t" org-timer-item)
     ("z" (org-info "Timers")))
-
-  ;; https://github.com/kaushalmodi/.emacs.d/blob/master/setup-files/setup-org.el#L330
-  (defun modi/org-in-any-block-p ()
-    "Return non-nil if the point is in any Org block.
-The Org block can be *any*: src, example, verse, etc., even any
-Org Special block.
-This function is heavily adapted from `org-between-regexps-p'."
-    (save-match-data
-      (let ((pos (point))
-            (case-fold-search t)
-            (block-begin-re "^[[:blank:]]*#\\+begin_\\(?1:.+?\\)\\(?: .*\\)*$")
-            (limit-up (save-excursion (outline-previous-heading)))
-            (limit-down (save-excursion (outline-next-heading)))
-            beg end)
-        (save-excursion
-          ;; Point is on a block when on BLOCK-BEGIN-RE or if
-          ;; BLOCK-BEGIN-RE can be found before it...
-          (and (or (org-in-regexp block-begin-re)
-                   (re-search-backward block-begin-re limit-up :noerror))
-               (setq beg (match-beginning 0))
-               ;; ... and BLOCK-END-RE after it...
-               (let ((block-end-re (concat "^[[:blank:]]*#\\+end_"
-                                           (match-string-no-properties 1)
-                                           "\\( .*\\)*$")))
-                 (goto-char (match-end 0))
-                 (re-search-forward block-end-re limit-down :noerror))
-               (> (setq end (match-end 0)) pos)
-               ;; ... without another BLOCK-BEGIN-RE in-between.
-               (goto-char (match-beginning 0))
-               (not (re-search-backward block-begin-re (1+ beg) :noerror))
-               ;; Return value.
-               (cons beg end))))))
-
-  (defun modi/org-split-block ()
-    "Sensibly split the current Org block at point.
-(1) Point in-between a line
-    #+begin_src emacs-lisp             #+begin_src emacs-lisp
-    (message▮ \"one\")                   (message \"one\")
-    (message \"two\")          -->       #+end_src
-    #+end_src                          ▮
-                                       #+begin_src emacs-lisp
-                                       (message \"two\")
-                                       #+end_src
-(2) Point at EOL
-    #+begin_src emacs-lisp             #+begin_src emacs-lisp
-    (message \"one\")▮                   (message \"one\")
-    (message \"two\")          -->       #+end_src
-    #+end_src                          ▮
-                                       #+begin_src emacs-lisp
-                                       (message \"two\")
-                                       #+end_src
-(3) Point at BOL
-    #+begin_src emacs-lisp             #+begin_src emacs-lisp
-    (message \"one\")                    (message \"one\")
-    ▮(message \"two\")          -->      #+end_src
-    #+end_src                          ▮
-                                       #+begin_src emacs-lisp
-                                       (message \"two\")
-                                       #+end_src
-"
-    (interactive)
-    (if (modi/org-in-any-block-p)
-        (save-match-data
-          (save-restriction
-            (widen)
-            (let ((case-fold-search t)
-                  (at-bol (bolp))
-                  block-start
-                  block-end)
-              (save-excursion
-                (re-search-backward "^\\(?1:[[:blank:]]*#\\+begin_.+?\\)\\(?: .*\\)*$" nil nil 1)
-                (setq block-start (match-string-no-properties 0))
-                (setq block-end (replace-regexp-in-string
-                                 "begin_" "end_" ;Replaces "begin_" with "end_", "BEGIN_" with "END_"
-                                 (match-string-no-properties 1))))
-              ;; Go to the end of current line, if not at the BOL
-              (unless at-bol
-                (end-of-line 1))
-              (insert (concat (if at-bol "" "\n")
-                              block-end
-                              "\n\n"
-                              block-start
-                              (if at-bol "\n" "")))
-              ;; Go to the line before the inserted "#+begin_ .." line
-              (beginning-of-line (if at-bol -1 0)))))
-      (message "Point is not in an Org block")))
-
-  (defun modi/org-meta-return (&optional arg)
-    "Insert a new heading or wrap a region in a table.
-Calls `org-insert-heading', `org-insert-item',
-`org-table-wrap-region', or `modi/org-split-block' depending on
-context.  When called with an argument, unconditionally call
-`org-insert-heading'."
-    (interactive "P")
-    (org-check-before-invisible-edit 'insert)
-    (or (run-hook-with-args-until-success 'org-metareturn-hook)
-        (call-interactively (cond (arg #'org-insert-heading)
-                                  ((org-at-table-p) #'org-table-wrap-region)
-                                  ((org-in-item-p) #'org-insert-item)
-                                  ((modi/org-in-any-block-p) #'modi/org-split-block)
-                                  (t #'org-insert-heading)))))
-  (advice-add 'org-meta-return :override #'modi/org-meta-return)
-
-  ;; Make C-u C-return insert heading *at point* (not respecting content),
-  ;; even when the point is directly after a list item.
-  ;; Reason: http://lists.gnu.org/r/emacs-orgmode/2018-02/msg00368.html
-  (defun modi/org-insert-heading-respect-content (&optional invisible-ok)
-    "Insert heading with `org-insert-heading-respect-content' set to t.
-With \\[universal-argument] prefix, insert Org heading directly at
-point."
-    (interactive)
-    (let ((respect-content (unless current-prefix-arg
-                             '(4))))
-      (org-insert-heading respect-content invisible-ok)))
-  (advice-add 'org-insert-heading-respect-content :override
-              #'modi/org-insert-heading-respect-content)
 
   ;; https://github.com/daviderestivo/galactic-emacs/blob/master/lisp/org-archive-subtree.el
   ;; archive subtrees/headings while also preserving their context
