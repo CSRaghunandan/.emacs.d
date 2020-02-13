cbe22a8c71742419c46390c3ec17c8f7e9cb9262
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-13 22:12:14 +0530

    Cut: delete unnecessary function
    Just use ivy to find file using sudo and open with tramp instead. Its much
    easier than going into read-only and editing a file. Also, this function doesn't
    work sometimes on files for which we don't have permission to edit.

--- setup-files/setup-editing.el
+++ setup-files/setup-editing.el
@@ -1,5 +1,5 @@
 ;;; setup-editing.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-02-13 21:32:24 csraghunandan>
+;; Time-stamp: <2020-02-13 22:12:09 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -556,35 +556,6 @@ this command."
       (flush-lines "^[[:blank:]]*$" (region-beginning) (region-end)))
     do-not-run-orig-fn))
 (advice-add 'delete-blank-lines :before-until #'modi/delete-blank-lines-in-region)
-
-(defadvice basic-save-buffer-2 (around fix-unwritable-save-with-sudo activate)
-  "When we save a buffer which is write-protected, try to sudo-save it.
-
-When the buffer is write-protected it is usually opened in
-read-only mode.  Use \\[read-only-mode] to toggle
-`read-only-mode', make your changes and \\[save-buffer] to save.
-Emacs will warn you that the buffer is write-protected and asks
-you to confirm if you really want to save.  If you answer yes,
-Emacs will use sudo tramp method to save the file and then
-reverts it, making it read-only again.  The buffer stays
-associated with the original non-sudo filename."
-           (condition-case err
-               (progn
-                 ad-do-it)
-             (file-error
-              (when (string-prefix-p
-                     "Doing chmod: operation not permitted"
-                     (error-message-string err))
-                (let ((old-buffer-file-name buffer-file-name)
-                      (success nil))
-                  (unwind-protect
-                      (progn
-                        (setq buffer-file-name (concat "/sudo::" buffer-file-name))
-                        (save-buffer)
-                        (setq success t))
-                    (setq buffer-file-name old-buffer-file-name)
-                    (when success
-                      (revert-buffer t t))))))))
 
 (defun kill-back-to-indentation ()
   "Kill from point back to the first non-whitespace character on the line."
