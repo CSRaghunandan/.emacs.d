e412ccf4b43adceef91a9247a33e50671317e017
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-13 20:59:35 +0530

    Add: modify the default behavior of avy
    Cut: remove broken functions in avy

--- setup-files/setup-avy.el
+++ setup-files/setup-avy.el
@@ -1,5 +1,5 @@
 ;;; setup-avy.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2019-01-09 00:40:26 csraghunandan>
+;; Time-stamp: <2020-02-13 20:56:34 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -9,23 +9,17 @@
 (use-package avy
   :bind
   (("C-'" . avy-goto-char-timer)
-   ("M-g M-g" . avy-goto-line)
-   ("M-g [" . avy-goto-paren-open)
-   ("M-g ]" . avy-goto-paren-close))
+   ("M-g M-g" . avy-goto-line))
   :bind*
   ("C-," . avy-goto-word-1)
   :config
   (setq avy-style 'pre)
-
-  (setq avy-indent-line-overlay t)
-
-  (defun avy-goto-paren-open ()
-    (interactive)
-    (avy--generic-jump "(\\|{\\|\\[" nil 'pre))
-
-  (defun avy-goto-paren-close ()
-    (interactive)
-    (avy--generic-jump ")\\|}\\|]" nil 'pre)))
+  (setq avy-background t
+        ;; the unpredictability of this (when enabled) makes it a poor default
+        avy-single-candidate-jump nil
+        avy-all-windows nil
+        avy-all-windows-alt t)
+  (setq avy-indent-line-overlay t))
 
 ;; ace-link: quickly traverse through links in info
 ;; https://github.com/abo-abo/ace-link
