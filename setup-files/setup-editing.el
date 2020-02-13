ac76644185013de2387311676854ab0975eac1e8
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-13 15:31:03 +0530

    Add: update xah-clean-empty-lines and xah-clean-whitespace

--- setup-files/setup-editing.el
+++ setup-files/setup-editing.el
@@ -1,5 +1,5 @@
 ;;; setup-editing.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-02-13 10:08:33 csraghunandan>
+;; Time-stamp: <2020-02-13 15:30:22 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -170,56 +170,53 @@ Position the cursor at it's beginning, according to the current mode."
   :config (save-place-mode))
 
 
-
-(defun xah-clean-empty-lines (&optional *begin *end *n)
+(defun xah-clean-empty-lines ()
   "Replace repeated blank lines to just 1.
 Works on whole buffer or text selection, respects `narrow-to-region'.
 
-*N is the number of newline chars to use in replacement.
-If 0, it means lines will be joined.
-By befault, *N is 2. It means, 1 visible blank line.
-
 URL `http://ergoemacs.org/emacs/elisp_compact_empty_lines.html'
-Version 2017-01-27"
-  (interactive
-   (if (region-active-p)
-       (list (region-beginning) (region-end))
-     (list (point-min) (point-max))))
-  (when (not *begin)
-    (setq *begin (point-min) *end (point-max)))
-  (save-excursion
-    (save-restriction
-      (narrow-to-region *begin *end)
-      (progn
-        (goto-char (point-min))
-        (while (re-search-forward "\n\n\n+" nil "NOERROR")
-          (replace-match (make-string (if *n *n 2) 10)))))))
+Version 2017-09-22"
+  (interactive)
+  (let ($begin $end)
+    (if (region-active-p)
+        (setq $begin (region-beginning) $end (region-end))
+      (setq $begin (point-min) $end (point-max)))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $begin $end)
+        (progn
+          (goto-char (point-min))
+          (while (re-search-forward "\n\n\n+" nil "move")
+            (replace-match "\n\n")))))))
 
-(defun xah-clean-whitespace (&optional *begin *end)
+(defun xah-clean-whitespace ()
   "Delete trailing whitespace, and replace repeated blank lines to just 1.
 Only space and tab is considered whitespace here.
 Works on whole buffer or text selection, respects `narrow-to-region'.
 
 URL `http://ergoemacs.org/emacs/elisp_compact_empty_lines.html'
-Version 2016-10-15"
-  (interactive
-   (if (region-active-p)
-       (list (region-beginning) (region-end))
-     (list (point-min) (point-max))))
-  (when (null *begin)
-    (setq *begin (point-min)  *end (point-max)))
-  (save-excursion
-    (save-restriction
-      (narrow-to-region *begin *end)
-      (progn
-        (goto-char (point-min))
-        (while (search-forward-regexp "[ \t]+\n" nil "noerror")
-          (replace-match "\n")))
-      (xah-clean-empty-lines (point-min) (point-max))
-      (progn
-        (goto-char (point-max))
-        (while (equal (char-before) 32) ; char 32 is space
-          (delete-char -1))))))
+Version 2017-09-22"
+  (interactive)
+  (let ($begin $end)
+    (if (region-active-p)
+        (setq $begin (region-beginning) $end (region-end))
+      (setq $begin (point-min) $end (point-max)))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $begin $end)
+        (progn
+          (goto-char (point-min))
+          (while (re-search-forward "[ \t]+\n" nil "move")
+            (replace-match "\n")))
+        (progn
+          (goto-char (point-min))
+          (while (re-search-forward "\n\n\n+" nil "move")
+            (replace-match "\n\n")))
+        (progn
+          (goto-char (point-max))
+          (while (equal (char-before) 32) ; char 32 is space
+            (delete-char -1))))
+      (message "white space cleaned"))))
 
 
 
