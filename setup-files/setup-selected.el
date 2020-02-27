6595b5a05b63441f80110531bb3914c203ac8d33
CSRaghunandan <rnraghunandan@gmail.com>
2020-02-27 15:19:37 +0530

    Add: indent-region to selected-keymap
    Fix: add correct function for duplicate-thing

--- setup-files/setup-selected.el
+++ setup-files/setup-selected.el
@@ -1,5 +1,5 @@
 ;;; setup-selected.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 03:07:48 csraghunandan>
+;; Time-stamp: <2020-02-27 15:18:52 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -11,10 +11,11 @@
               ("w" . kill-region)
               ("~" . hydra-change-case/body)
               ("c" . copy-region-as-kill)
-              ("d" . duplicate-current-line-or-region)
+              ("d" . duplicate-thing)
               ("E" . eval-region)
               ("e" . er/expand-region)
               ("f" . fill-region)
+              ("/" . indent-region)
               ("q" . vr/query-replace)
               (";" . comment-or-uncomment-region)
               ("s" . rag/kill-rectangle-replace-with-space)
