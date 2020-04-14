80ef0ad44921703f04c697b1b46445dba32c6481
CSRaghunandan <rnraghunandan@gmail.com>
2020-04-14 15:28:44 +0530

    Add: go back to using swiper as the default search tool instead of swiper-isearch
    swiper-isearch is causing emacs28 to hang

--- setup-files/setup-swiper.el
+++ setup-files/setup-swiper.el
@@ -1,5 +1,5 @@
 ;;; setup-swiper.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-02-24 23:40:29 csraghunandan>
+;; Time-stamp: <2020-04-14 15:27:20 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -7,27 +7,11 @@
 ;; swiper: isearch with an overview!
 ;; https://github.com/abo-abo/swiper
 (use-package swiper
-  :bind (("C-s" . counsel-grep-or-isearch-swiper))
+  :bind (("C-s" . counsel-grep-or-swiper))
   :config
   (setq swiper-action-recenter t
         ;; Jump to the beginning of match when leaving Swiper
-        swiper-goto-start-of-match t)
-
-;;;###autoload
-  (defun counsel-grep-or-isearch-swiper (&optional initial-input)
-    "Call `swiper' for small buffers and `counsel-grep' for large ones.
-When non-nil, INITIAL-INPUT is the initial search pattern."
-    (interactive)
-    (if (or (not buffer-file-name)
-            (buffer-narrowed-p)
-            (ignore-errors
-              (file-remote-p buffer-file-name))
-            (jka-compr-get-compression-info buffer-file-name)
-            (funcall counsel-grep-use-swiper-p))
-        (swiper-isearch initial-input)
-      (when (file-writable-p buffer-file-name)
-        (save-buffer))
-      (counsel-grep initial-input))))
+        swiper-goto-start-of-match t))
 
 (provide 'setup-swiper)
 
