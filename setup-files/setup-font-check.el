00cdeebe62e219c02382d6a58cdc98dcac47dc41
CSRaghunandan <rnraghunandan@gmail.com>
2020-05-05 12:03:14 +0530

    Add: Used medium weight iosevka font as default

--- setup-files/setup-font-check.el
+++ setup-files/setup-font-check.el
@@ -1,5 +1,5 @@
 ;;; setup-font-check.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-01-27 18:10:53 csraghunandan>
+;; Time-stamp: <2020-05-05 11:59:31 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -13,8 +13,8 @@
         (when (member "Iosevka SS08" (font-family-list))
           (progn
             (if (is-linux-p)
-                (set-frame-font "Iosevka SS08-10" nil t)
-              (set-frame-font "Iosevka SS08-12" nil t)))))))
+                (set-frame-font "Iosevka SS08 Medium-10" nil t)
+              (set-frame-font "Iosevka SS08 Medium-12" nil t)))))))
 
 (add-hook 'after-make-frame-functions #'rag-set-face)
 
@@ -22,7 +22,7 @@
 (when (member "Iosevka SS08" (font-family-list))
   (progn
     (if (is-linux-p)
-        (set-frame-font "Iosevka SS08-10" nil t)
-      (set-frame-font "Iosevka SS08-12" nil t))))
+        (set-frame-font "Iosevka SS08 Medium-10" nil t)
+      (set-frame-font "Iosevka SS08 Medium-12" nil t))))
 
   (provide 'setup-font-check)
