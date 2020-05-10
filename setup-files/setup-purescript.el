91c6c92312b6693f44f4e0f89180e1c6a56a4faf
CSRaghunandan <rnraghunandan@gmail.com>
2020-05-10 12:07:12 +0530

    Add: use lsp for purescript

--- setup-files/setup-purescript.el
+++ setup-files/setup-purescript.el
@@ -1,5 +1,5 @@
 ;;; setup-purescript.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 03:05:58 csraghunandan>
+;; Time-stamp: <2020-05-10 12:02:20 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -9,36 +9,17 @@
 ;; purescript-mode: major mode for editing purescript files
 ;; https://github.com/dysinger/purescript-mode
 (use-package purescript-mode :defer t
+  :hook ((purescript-mode . turn-on-purescript-decl-scan)
+         (purescript-mode . (lambda ()
+                              (lsp-deferred)
+                              (lsp-ui-sideline-mode)
+                              (lsp-ui-sideline-toggle-symbols-info)
+                              (company-mode))))
   :config
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-decl-scan)
-
-  ;; psc-ide: provide IDE like features for purescript
-  ;; https://github.com/epost/psc-ide-emacs
-  (use-package psc-ide
-    :config
-    (add-hook 'purescript-mode-hook
-              (lambda ()
-                (psc-ide-mode)
-                (company-mode)
-                (flycheck-mode)
-                (turn-on-purescript-indentation)))
-
-    (add-hook 'purescript-mode-hook
-              (lambda ()
-                (add-hook 'before-save-hook
-                          (lambda ()
-                            (time-stamp)
-                            (xah-clean-whitespace)) nil t)))))
+  (defun my-purescript-mode-hook()
+    (set (make-local-variable 'company-backends)
+         '((company-capf company-files :with company-yasnippet)
+           (company-dabbrev-code company-dabbrev))))
+  (add-hook 'purescript-mode-hook #'my-purescript-mode-hook))
 
 (provide 'setup-purescript)
-
-;; C-c C-s	psc-ide-server-start
-;; C-c C-q	psc-ide-server-quit
-;; C-c C-t	psc-ide-show-type
-;; C-c C-i	psc-ide-add-import
-;; C-c C-a	psc-ide-add-clause
-;; C-c C-c	psc-ide-case-split
-;; C-c C-l	psc-ide-load-all
-;; C-c C-b	psc-ide-rebuild
-;; C-c C-S-l	psc-ide-load-module
-;; C-c M-s	psc-ide-flycheck-insert-suggestion
