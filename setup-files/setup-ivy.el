55d2e533a204c34113df1ee4fb121c899fc56126
CSRaghunandan <rnraghunandan@gmail.com>
2020-05-10 12:28:16 +0530

    Add: start using swiper-isearch again
    Did not notice emacs hanging up on latest version of org

--- setup-files/setup-ivy.el
+++ setup-files/setup-ivy.el
@@ -1,5 +1,5 @@
 ;;; setup-ivy.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-05-09 18:35:23 csraghunandan>
+;; Time-stamp: <2020-05-10 11:44:18 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -15,7 +15,7 @@
 ;; `M-n' -> next search item
 ;; `M-n' -> to select the symbol at point in swiper
 (use-package swiper
-  :bind (("C-s" . counsel-grep-or-swiper))
+  :bind (("C-s" . swiper-isearch))
   :config
   (setq swiper-action-recenter t
         ;; Jump to the beginning of match when leaving Swiper
