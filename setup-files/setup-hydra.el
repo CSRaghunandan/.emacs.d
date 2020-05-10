68f5c52178b02ad4519035ce048373509b7d7285
CSRaghunandan <rnraghunandan@gmail.com>
2020-05-10 12:27:26 +0530

    Add: hydra for straight.el

--- setup-files/setup-hydra.el
+++ setup-files/setup-hydra.el
@@ -1,5 +1,5 @@
 ;;; setup-hydra.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 02:58:15 csraghunandan>
+;; Time-stamp: <2020-05-10 12:27:16 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -10,9 +10,41 @@
 (use-package hydra
   :config (hydra-add-font-lock))
 
+(defhydra hydra-straight-helper (:hint nil)
+  "
+_c_heck all       |_f_etch all     |_m_erge all      |_n_ormalize all   |p_u_sh all
+_C_heck package   |_F_etch package |_M_erge package  |_N_ormlize package|p_U_sh package
+----------------^^+--------------^^+---------------^^+----------------^^+------------||_q_uit||
+_r_ebuild all     |_p_ull all      |_v_ersions freeze|_w_atcher start   |_g_et recipe
+_R_ebuild package |_P_ull package  |_V_ersions thaw  |_W_atcher quit    |prun_e_ build"
+  ("c" straight-check-all)
+  ("C" straight-check-package)
+  ("r" straight-rebuild-all)
+  ("R" straight-rebuild-package)
+  ("f" straight-fetch-all)
+  ("F" straight-fetch-package)
+  ("p" straight-pull-all)
+  ("P" straight-pull-package)
+  ("m" straight-merge-all)
+  ("M" straight-merge-package)
+  ("n" straight-normalize-all)
+  ("N" straight-normalize-package)
+  ("u" straight-push-all)
+  ("U" straight-push-package)
+  ("v" straight-freeze-versions)
+  ("V" straight-thaw-versions)
+  ("w" straight-watcher-start)
+  ("W" straight-watcher-quit)
+  ("g" straight-get-recipe)
+  ("e" straight-prune-build)
+  ("q" nil))
+
+(bind-key "C-c h p" 'hydra-straight-helper/body)
+
 (provide 'setup-hydra)
 
 ;; hydras: ibuffer, origami, move, yasnippet, info, macros, diff-hl dired sort,
 ;;         bm, toggle case, langtools, kruecolor smartparens, multi-term,
 ;;         aprops, quickrun, org-clock, command-log info-to, js2-refactor,
-;;         mark-org-table, smerge-mode, font-resize pdftools, timestamps, flycheck
+;;         mark-org-table, smerge-mode, font-resize pdftools, timestamps,
+;;         flycheck, straight, transpose-frame
