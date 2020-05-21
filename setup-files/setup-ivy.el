c017cb36c7fc98b6bc7e26bad9b50cb227faab02
CSRaghunandan <rnraghunandan@gmail.com>
2020-05-21 23:48:34 +0530

    Add: use counsel-grep-or-swiper as the default search
    swiper-isearch is still causing search on org files to hang my emacs session

--- setup-files/setup-ivy.el
+++ setup-files/setup-ivy.el
@@ -1,5 +1,5 @@
 ;;; setup-ivy.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-05-17 14:06:24 csraghunandan>
+;; Time-stamp: <2020-05-21 23:48:04 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -15,7 +15,7 @@
 ;; `M-n' -> next search item
 ;; `M-n' -> to select the symbol at point in swiper
 (use-package swiper
-  :bind (("C-s" . swiper-isearch))
+  :bind (("C-s" . counsel-grep-or-swiper))
   :config
   (setq swiper-action-recenter t
         ;; Jump to the beginning of match when leaving Swiper
