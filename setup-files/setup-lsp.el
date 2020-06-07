8e53eccd265d1fa90de7e49cead66ed9270609b1
CSRaghunandan <rnraghunandan@gmail.com>
2020-06-07 23:58:49 +0530

    Add: minor changes to lsp-ui

--- setup-files/setup-lsp.el
+++ setup-files/setup-lsp.el
@@ -1,5 +1,5 @@
 ;;; setup-lsp.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2020-05-31 22:41:07 csraghunandan>
+;; Time-stamp: <2020-06-07 23:56:53 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -35,6 +35,8 @@
         lsp-ui-sideline-show-code-actions t
         lsp-ui-sideline-show-hover t
         lsp-ui-doc-enable nil
+        lsp-ui-doc-include-signature t
+        lsp-eldoc-enable-hover nil ; Disable eldoc displays in minibuffer
         lsp-ui-imenu-enable t
         lsp-ui-sideline-ignore-duplicate t))
 
