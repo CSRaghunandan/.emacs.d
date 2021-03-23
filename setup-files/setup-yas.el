c4694ae77833826409f4d596ff74655fb1e0ddfd
CSRaghunandan <rnraghunandan@gmail.com>
2021-03-24 04:30:07 +0530

    fix: runtime warning when loading yas

--- setup-files/setup-yas.el
+++ setup-files/setup-yas.el
@@ -1,5 +1,5 @@
 ;;; setup-yas.el -*- lexical-binding: t; -*-
-;; Time-stamp: <2018-08-15 13:32:59 csraghunandan>
+;; Time-stamp: <2021-03-24 02:56:36 csraghunandan>
 
 ;; Copyright (C) 2016-2020 Chakravarthy Raghunandan
 ;; Author: Chakravarthy Raghunandan <rnraghunandan@gmail.com>
@@ -18,11 +18,11 @@
   :init
   ;; Ensure `yas-reload-all' is called as late as possible. Other modules could
   ;; have additional configuration for yasnippet. For example, file-templates.
-  (add-transient-hook! 'yas-minor-mode-hook (yas-reload-all))
+  (add-transient-hook! #'yas-minor-mode-hook (yas-reload-all))
   :config
   (setq yas-triggers-in-field t) ; Enable nested triggering of snippets
-  (setq yas-prompt-functions '(yas-completing-prompt))
-  (add-hook 'snippet-mode-hook '(lambda () (setq-local require-final-newline nil)))
+  (setq yas-prompt-functions #'(yas-completing-prompt))
+  (add-hook 'snippet-mode-hook #'(lambda () (setq-local require-final-newline nil)))
 
   (bind-key "C-c y"
             (defhydra hydra-yas (:color blue
